6901bb1b72a2e9ee2c7748f1de814f45ed0460ab
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2022-10-07 16:59:54 +0200

    fix: eslint --fix

--- server/src/demarche/demarche.service.ts
+++ server/src/demarche/demarche.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 import { DsApiClient } from "@lab-mi/ds-api-client";
 
 @Injectable()
 export class DemarcheService {
-    async getDemarche(id: number): Promise<{demarche: Partial<any>}> {
-        const dsApiClient = new DsApiClient(process.env.API_URL, process.env.API_TOKEN);
-        return await dsApiClient.demarche(id);
-    }
+  async getDemarche(id: number): Promise<{ demarche: Partial<any> }> {
+    const dsApiClient = new DsApiClient(
+      process.env.API_URL,
+      process.env.API_TOKEN,
+    );
+    return await dsApiClient.demarche(id);
+  }
 }
