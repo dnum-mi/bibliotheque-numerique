4f270c093b8f11bbb679248e37a3c6a027b5a01e
pengfei.dong <mail@peng-fei.name>
2022-10-21 16:02:28 +0200

    add Dossier Vue for show detail of dossier

---
+++ client/src/views/Dossier.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router'
+
+import { computed, onMounted, ref, watch } from 'vue'
+import { useDossierStore } from '@/stores/dossier'
+
+const dossierStore = useDossierStore()
+const idDossier = ref(1)
+
+const idD = computed<string>(() => dossierStore.dossier?.id || '')
+const champsD = computed<string>(() => dossierStore.dossier?.champs || '')
+const demandeurD = computed<string>(() => dossierStore.dossier?.demandeur || '')
+
+watch(idDossier, async (value: number) => {
+  await dossierStore.getDossier(value)
+})
+
+onMounted(async () => {
+  // const { id } = useRoute().params
+  const params = useRoute()?.params
+  if (params && params.id) { idDossier.value = Number(params.id) }
+  await dossierStore.getDossier(idDossier.value)
+})
+
+</script>
+
+<template>
+  <h1>Dossier</h1>
+  Dossier ID: {{ idD }}
+
+  <h2>
+    Identité du demandeur:
+  </h2>
+  <div>
+    {{ demandeurD }}
+  </div>
+  <h2>
+    Formulaire
+  </h2>
+  <div>
+    {{ champsD }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ client/src/views/__tests__/dossier.js
@@ -0,0 +1,10 @@
+export const dossier = {
+  id: 1,
+  champs: [{ id: 123, label: 'nom', stringValue: 'test' }, { id: 124, label: 'prenom', stringValue: 'test' }],
+  demandeur: {
+    civilite: 'M',
+    nom: 'dong',
+    prenom: 'pengfei',
+    dateDeNaissance: null,
+  },
+}
