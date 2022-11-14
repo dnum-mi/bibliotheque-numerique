6a98d01edde9f04e3a24c47e38783a5cdab007b7
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2022-11-14 18:19:58 +0100

    feat: #69 Add error handling for dossier

--- server/src/dossiers/dossiers.service.ts
+++ server/src/dossiers/dossiers.service.ts
@@ -1,27 +1,74 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
 import { Dossier } from "../entities";
+import { LoggerService } from "../logger/logger.service";
 
 @Injectable()
 export class DossiersService {
-  findAll() {
-    return Dossier.all();
+  private readonly logger = new Logger(
+    DossiersService.name,
+  ) as unknown as LoggerService;
+
+  async findAll() {
+    try {
+      return await Dossier.all();
+    } catch (error) {
+      this.logger.error({
+        short_message: "Échec récupération des dossiers",
+        full_message: error.toString(),
+      });
+      throw new Error("Unable to retrieve dossiers");
+    }
   }
 
-  findOne(id: number) {
-    return Dossier.findOneBy({ id: id });
+  async findOne(id: number) {
+    try {
+      return await Dossier.findOneBy({ id: id });
+    } catch (error) {
+      this.logger.error({
+        short_message: `Échec récupération du dossier id: ${id}`,
+        full_message: error.toString(),
+      });
+      throw new Error("Unable to retrieve dossiers");
+    }
   }
 
-  findOneWithDetail(id: number) {
-    return Dossier.findOne({
-      where: { id: id },
-      relations: { dossierDS: true },
-    });
+  async findOneWithDetail(id: number) {
+    try {
+      return await Dossier.findOne({
+        where: { id: id },
+        relations: { dossierDS: true },
+      });
+    } catch (error) {
+      this.logger.error({
+        short_message: `Échec récupération du dossier id: ${id}`,
+        full_message: error.toString(),
+      });
+      throw new Error(`Unable to retrieve dossier id: ${id}`);
+    }
   }
 
   async remove(id: number) {
-    const dossier = await Dossier.findOneBy({ id: id });
+    let dossier: Dossier;
+    try {
+      dossier = await Dossier.findOneBy({ id: id });
+    } catch (error) {
+      this.logger.error({
+        short_message: `Échec récupération du dossier id: ${id}`,
+        full_message: error.toString(),
+      });
+      throw new Error(`Unable to retrieve dossier id: ${id}`);
+    }
+
     if (dossier) {
-      return dossier.remove();
+      try {
+        return await dossier.remove();
+      } catch (error) {
+        this.logger.error({
+          short_message: `Échec suppression du dossier id: ${id}`,
+          full_message: error.toString(),
+        });
+        throw new Error(`Unable to remove dossier id: ${id}`);
+      }
     }
   }
 }
