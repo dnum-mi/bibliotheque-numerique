eb6e1f85a5f3eb6bd74b13fa213698bf846802a7
baudointran <baudointran@users.noreply.github.com>
2022-11-21 12:01:41 +0100

    feat: add filter date in ag-grid

--- client/src/components/typeDataTable.d.ts
+++ client/src/components/typeDataTable.d.ts
@@ -2,6 +2,7 @@ export type TypeHeaderDataTable = {
   text: string,
   value?: string,
   sortable?: boolean,
+  type?: string,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   parseFn?: (value: any) => string
 }
