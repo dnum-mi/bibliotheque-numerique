dc285a54fa42fb04c99bb7c87ad12db2aaeafc86
baudointran <baudointran@users.noreply.github.com>
2022-11-30 22:12:39 +0100

    unit test back for dossiers controller

--- server/src/dossiers/dossiers.controller.spec.ts
+++ server/src/dossiers/dossiers.controller.spec.ts
@@ -4,6 +4,7 @@ import { DossiersService } from "./dossiers.service";
 
 describe("DossiersController", () => {
   let controller: DossiersController;
+  let service: DossiersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +13,80 @@ describe("DossiersController", () => {
     }).compile();
 
     controller = module.get<DossiersController>(DossiersController);
+    service = module.get<DossiersService>(DossiersService);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
   });
+
+  it.each`
+    name                   | servicefn              | fn
+    ${"findAll"}           | ${"findAll"}           | ${() => controller.findAll()}
+    ${"findOne"}           | ${"findOne"}           | ${() => controller.findOne("TEST")}
+    ${"findOneWithDetail"} | ${"findOneWithDetail"} | ${() => controller.findOneWithDetail("TEST")}
+    ${"search"}            | ${"findAll"}           | ${() => controller.searchDossier({})}
+  `("$name: should get dossiers ", async ({ name, servicefn, fn }) => {
+    const result = ["dossiers"];
+    jest.spyOn(service, servicefn).mockImplementation(() => result);
+    expect(await fn()).toBe(result);
+  });
+
+  it("findAll: should throw error when there are nothing", async () => {
+    jest.spyOn(service, "findAll").mockImplementation(async () => []);
+    await expect(() => controller.findAll()).rejects.toThrow(
+      "No dossier found",
+    );
+  });
+
+  it.each`
+    name                   | servicefn              | fn
+    ${"findOne"}           | ${"findOne"}           | ${() => controller.findOne("TEST")}
+    ${"findOneWithDetail"} | ${"findOneWithDetail"} | ${() => controller.findOneWithDetail("TEST")}
+  `(
+    "$name: should throw error when there are nothing",
+    async ({ name, servicefn, fn }) => {
+      jest.spyOn(service, servicefn).mockImplementation(() => null);
+      await expect(fn).rejects.toThrow("Dossier id: TEST not found");
+    },
+  );
+
+  it("search: should throw error when there are nothing", async () => {
+    const result = [];
+    jest.spyOn(service, "findAll").mockImplementation(async () => result);
+    expect(await controller.searchDossier({})).toBe(result);
+  });
+
+  it.each`
+    name                   | servicefn              | fn
+    ${"findAll"}           | ${"findAll"}           | ${() => controller.findAll()}
+    ${"findOne"}           | ${"findOne"}           | ${() => controller.findOne("TEST")}
+    ${"findOneWithDetail"} | ${"findOneWithDetail"} | ${() => controller.findOneWithDetail("TEST")}
+    ${"search"}            | ${"findAll"}           | ${() => controller.searchDossier({})}
+  `(
+    "$name: should throw error with message of error when there are error",
+    async ({ name, servicefn, fn }) => {
+      const message = `Test Error ${name}`;
+      jest.spyOn(service, servicefn).mockImplementation(() => {
+        throw new Error(message);
+      });
+      await expect(fn).rejects.toThrow(message);
+    },
+  );
+
+  it.each`
+    name                   | servicefn              | fn
+    ${"findAll"}           | ${"findAll"}           | ${() => controller.findAll()}
+    ${"findOne"}           | ${"findOne"}           | ${() => controller.findOne("TEST")}
+    ${"findOneWithDetail"} | ${"findOneWithDetail"} | ${() => controller.findOneWithDetail("TEST")}
+    ${"search"}            | ${"findAll"}           | ${() => controller.searchDossier({})}
+  `(
+    "$name: should throw error with message 'internal error' when there are unknown error",
+    async ({ name, servicefn, fn }) => {
+      jest.spyOn(service, servicefn).mockImplementation(() => {
+        throw "Test Error";
+      });
+      await expect(fn).rejects.toThrow("Internal Server Error");
+    },
+  );
 });
