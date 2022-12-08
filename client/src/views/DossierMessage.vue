92b086d3d8dd13e2acf32060d724908ba5baa8c9
baudointran <baudointran@users.noreply.github.com>
2022-12-08 17:16:41 +0100

    feat: add vue component messageires

---
+++ client/src/views/DossierMessage.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { dateTimeToStringFr } from '@/utils/dateToString'
+
+defineProps<{
+    email: string,
+    body: string,
+    createdAt: string,
+  }>()
+
+</script>
+
+<template>
+  <div
+    class="fr-callout"
+    style="width: 100%;"
+  >
+    <p class="fr-callout__title">
+      {{ email }}
+    </p>
+    <p class="fr-callout__text">
+      <span v-html="body" />
+    </p>
+    <p>{{ dateTimeToStringFr(createdAt) }}</p>
+  </div>
+</template>
---
+++ client/src/views/DossierMessages.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+import DossierMessage from './DossierMessage.vue'
+
+const props = withDefaults(defineProps<{
+    datas?: object
+  }>(), {
+  datas: () => ({}),
+})
+const messages = computed(() => props.datas?.messages || [])
+</script>
+
+<template>
+  <div
+    v-if="datas"
+    class="fr-container"
+  >
+    <div class="fr-grid-row">
+      <div
+        v-for="{ id, email, body, createdAt } in messages"
+        :key="id"
+        class="fr-col-12 fr-grid-row"
+      >
+        <DossierMessage
+          data-cy="dossier-message"
+          :email="email"
+          :body="body"
+          :created-at="createdAt"
+        />
+      </div>
+    </div>
+  </div>
+</template>
