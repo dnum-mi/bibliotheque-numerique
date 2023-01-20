fc71881177b19d98e507de61459746c99a3d25b7
Bastien CHEVALLIER <cheval_b@etna-alternance.net>
2023-01-20 15:02:52 +0100

    feat: #116 Add test for User & Admin page

---
+++ client/src/views/User.cy.ts
@@ -0,0 +1,50 @@
+import '@gouvminint/vue-dsfr/styles'
+import '../main.css'
+import * as icons from '../icons'
+
+import VueDsfr from '@gouvminint/vue-dsfr'
+
+import { createPinia } from 'pinia'
+import User from './User.vue'
+
+import { createRandomAdmin, createRandomUser } from './__tests__/users'
+import { createRandomRoles } from '@/views/__tests__/roles'
+import { useRoleStore, useUserStore } from '@/stores'
+
+describe.only('<User />', () => {
+  it('Should render because admin', () => {
+    const pinia = createPinia()
+
+    const userStore = useUserStore(pinia)
+    const roleStore = useRoleStore(pinia)
+
+    userStore.currentUser = createRandomAdmin()
+    userStore.users = new Map()
+    roleStore.roles = createRandomRoles(10)
+
+    userStore.loadUserById = (id: number) => {
+      userStore.users.set(id, { ...createRandomUser(), id })
+      return Promise.resolve()
+    }
+    roleStore.getRoles = () => Promise.resolve()
+
+    const extensions = {
+      use: [
+        pinia,
+        {
+          install: (app) => {
+            app.use(VueDsfr,
+              { icons: Object.values(icons) },
+            )
+          },
+        },
+      ],
+    }
+    cy.mount(User, {
+      extensions,
+    })
+
+    cy.get('h2.mb-20').should('contain', 'Utilisateur')
+    cy.get('[data-cy=user-role-list] [data-cy=cell-action-icon]').should('have.length', 10)
+  })
+})
