bee2e97d181c23ce26fe5138f49f71969fa83431
baudointran <baudointran@users.noreply.github.com>
2023-01-27 18:01:50 +0100

    fix: to run unit test

--- server/src/plugins/organisme/organismes_datas/organismes_datas.controller.spec.ts
+++ server/src/plugins/organisme/organismes_datas/organismes_datas.controller.spec.ts
@@ -1,4 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConnectorModule } from "../../connector/connector.module";
+import { OrganismesData, OrganismesSource } from "../entities";
+import { datasourceTest } from "../entities/__tests__";
 import { OrganismesDatasController } from "./organismes_datas.controller";
 import { OrganismesDatasService } from "./organismes_datas.service";
 
@@ -7,6 +11,13 @@ describe("OrganismesDatasController", () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(
+          datasourceTest([OrganismesData, OrganismesSource]).options,
+        ),
+        ConnectorModule.register(OrganismesSource),
+      ],
+
       controllers: [OrganismesDatasController],
       providers: [OrganismesDatasService],
     }).compile();
