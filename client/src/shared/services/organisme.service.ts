919551f35b86343e09147ad789d3f34e5712690f
baudointran <baudointran@users.noreply.github.com>
2023-03-03 17:55:15 +0100

    fix: format for RNA V1

--- client/src/shared/services/organisme.service.ts
+++ client/src/shared/services/organisme.service.ts
@@ -25,38 +25,34 @@ export async function fetchOrganimseByIdRNA (id: string): Promise<any | null> {
       headers,
     }
     const response = await axios(config)
-
-    // const data = {
-    //   organismeData: {
-    //     dataJson: {
-    //       data: {
-    //         rna_id: asso.id_association,
-    //         titre: asso.titre,
-    //         objet: asso.objet,
-    //         siret: asso.siret,
-    //         siret_siege_social: '',
-    //         date_creation: asso.date_creation,
-    //         date_declaration: asso.date_derniere_declaration,
-    //         date_publication: asso.date_publication_creation,
-    //         date_dissolution: asso.date_declaration_dissolution,
-    //         adresse_siege: {
-    //           complement: '',
-    //           numero_voie: asso.adresse_numero_voie,
-    //           type_voie: asso.adresse_type_voie,
-    //           libelle_voie: asso.adresse_libelle_voie,
-    //           distribution: asso.adresse_distribution,
-    //           code_insee: asso.adresse_code_insee,
-    //           code_postal: asso.adresse_code_postal,
-    //           commune: asso.adresse_libelle_commune,
-    //         },
-    //         etat: true,
-    //         groupement: null,
-    //         mise_a_jour: asso.derniere_maj,
-    //       },
-    //     },
-    //   },
-    // }
-
+    const data = response.data
+    if (data.organismesSource === 'API_RNA_V1') {
+      const dataJson = {
+        rna_id: data.dataJson.id_association,
+        titre: data.dataJson.titre,
+        objet: data.dataJson.objet,
+        siret: data.dataJson.siret,
+        siret_siege_social: '',
+        date_creation: data.dataJson.date_creation,
+        date_declaration: data.dataJson.date_derniere_declaration,
+        date_publication: data.dataJson.date_publication_creation,
+        date_dissolution: data.dataJson.date_declaration_dissolution,
+        adresse_siege: {
+          complement: '',
+          numero_voie: data.dataJson.adresse_numero_voie,
+          type_voie: data.dataJson.adresse_type_voie,
+          libelle_voie: data.dataJson.adresse_libelle_voie,
+          distribution: data.dataJson.adresse_distribution,
+          code_insee: data.dataJson.adresse_code_insee,
+          code_postal: data.dataJson.adresse_code_postal,
+          commune: data.dataJson.adresse_libelle_commune,
+        },
+        etat: true,
+        groupement: null,
+        mise_a_jour: data.dataJson.derniere_maj,
+      }
+      data.dataJson = dataJson
+    }
     // return data
     return response.data
   } catch (error) {
