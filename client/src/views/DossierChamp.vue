338ad5745f73a6348e38582e19b8054b0c94c3cd
pengfei.dong <mail@peng-fei.name>
2023-03-10 16:25:11 +0100

    use new style for vue dossier champs

--- client/src/views/DossierChamp.vue
+++ client/src/views/DossierChamp.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
 import DossierChamps from './DossierChamps.vue'
+import { prettyByteSizeByString } from '../utils/prettyByteSize'
+
 withDefaults(defineProps<{
     champ?: object[]
   }>(), {
@@ -12,30 +14,34 @@ const dataCy = 'dossier-champ'
   <div
     v-if="champ"
     :data-cy="dataCy"
-    class="fr-mb-2w"
+    class="fr-mb-2w fr-col-12"
   >
+    <hr class="fr-mt-3w fr-pb-1w">
     <label
       :for="champ.id"
-      class="fr-text--bold fr-col-6"
-    > {{ champ.label }} : </label>
+      class="fr-text--bold fr-text--lg bn-champ--text"
+    > {{ champ.label }}</label>
 
-    <span
+    <div
       v-if="champ.__typename =='PieceJustificativeChamp' "
       :id="champ.id"
-      class="fr-text fr-col-6"
-    > <a
-      :href=" champ.file.url"
-      target="_blank"
-    >{{ champ.file.filename }}</a>
-    </span>
+      class="fr-text fr-mt-1w bn-champ--text"
+    >
+      <a
+        :href=" champ.file.url"
+        target="_blank"
+      >{{ champ.file.filename }} <span class="fr-text--xs">({{ prettyByteSizeByString(champ.file.byteSizeBigInt) }})</span></a>
+    </div>
     <DossierChamps
       v-else-if="champ.__typename =='RepetitionChamp' "
       :champs="champ.champs"
     />
-    <span
+    <div
       v-else
       :id="champ.id"
-      class="fr-text fr-col-6"
-    > {{ champ.stringValue }}</span>
+      class="fr-text fr-mt-1w bn-champ--text"
+    >
+      {{ champ.stringValue }}
+    </div>
   </div>
 </template>
