ca4ed684996542ba2d2374f908fe2b4b3b579711
baudointran <baudointran@users.noreply.github.com>
2023-04-24 10:40:31 +0200

    fix: key intruction in variable

--- apps/server/src/plugins/instruction_time/config/instructionTimeMapping.config.ts
+++ apps/server/src/plugins/instruction_time/config/instructionTimeMapping.config.ts
@@ -1,11 +1,26 @@
+export const keyInstructionTime = {
+  DATE_REQUEST1: "DateRequest1",
+  DATE_RECEIPT1: "DateReceipt1",
+  BEGIN_PROROGATION_DATE: "BeginProrogationDate",
+  DURATION_EXTENSION: "DurationExtension",
+  DATE_REQUEST2: "DateRequest2",
+  DATE_RECEIPT2: "DateReceipt2",
+  DATE_INTENT_OPPOSITION: "DateIntentOpposition",
+} as const;
+
 const config = {
   instructionTimeMappingConfig: {
-    DateRequest1: "Date de la première demande de pièces",
-    DateReceipt1: "Date de réception des pièces de la première demande",
-    BeginProrogationDate: "Date de début de prorogation",
-    DurationExtension: "Durée de la prorogation",
-    DateRequest2: "Date de deuxième demande de pièces complémentaires",
-    DateReceipt2: "Date de réception des pièces de la deuxième demande",
+    [keyInstructionTime.DATE_REQUEST1]: "Date de la première demande de pièces",
+    [keyInstructionTime.DATE_RECEIPT1]:
+      "Date de réception des pièces de la première demande",
+    [keyInstructionTime.BEGIN_PROROGATION_DATE]: "Date de début de prorogation",
+    [keyInstructionTime.DURATION_EXTENSION]: "Durée de la prorogation",
+    [keyInstructionTime.DATE_REQUEST2]:
+      "Date de deuxième demande de pièces complémentaires",
+    [keyInstructionTime.DATE_RECEIPT2]:
+      "Date de réception des pièces de la deuxième demande",
+    [keyInstructionTime.DATE_INTENT_OPPOSITION]:
+      "Date de l'intention d'opposition aux financements",
   },
 };
 
