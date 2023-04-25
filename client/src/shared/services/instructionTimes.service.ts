750e834d19e3cc1c92e1b7a698cdfbf4878be374
pengfei.dong <mail@peng-fei.name>
2023-04-25 13:33:12 +0200

    debug url client for instruction time

--- client/src/shared/services/instructionTimes.service.ts
+++ client/src/shared/services/instructionTimes.service.ts
@@ -3,7 +3,7 @@ import { baseApiUrl, headers } from '@/utils/api-client'
 import type { TypeInstructionTimes } from '../types/instructionTime.type'
 
 const axiosInstructionTimes = axios.create({
-  baseURL: `${baseApiUrl}/instruction-times"`,
+  baseURL: `${baseApiUrl}/instruction-times`,
   headers,
 })
 
