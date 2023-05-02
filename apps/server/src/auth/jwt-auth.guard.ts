d18215b706a5275b820a32ccf6e57f069612abd1
Stanislas Ormières <stan@stormier.ninja>
2023-05-02 15:52:13 +0200

    fix(JwtAuthGuard): :bug: Fix type annotation

--- apps/server/src/auth/jwt-auth.guard.ts
+++ apps/server/src/auth/jwt-auth.guard.ts
@@ -4,10 +4,13 @@ import {
   UnauthorizedException,
 } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {
-  canActivate(context: ExecutionContext) {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     // TODO: Add your custom authentication logic here
     // for example, call super.logIn(request) to establish a session.
     return super.canActivate(context);
