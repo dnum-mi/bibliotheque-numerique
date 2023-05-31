2c96b29d4ce0dedb67ca0d4548d1767f8bc54478
Robin <pulsarr@pulsarrookprom2.home>
2023-05-31 12:36:35 +0200

    reputting joblogmodule

--- apps/server/src/modules/cron/cron.module.ts
+++ apps/server/src/modules/cron/cron.module.ts
@@ -2,13 +2,14 @@ import { Module } from "@nestjs/common";
 import { CronService } from "./cron.service";
 import { LoggerModule } from "../logger/logger.module";
 import { DossiersModule } from "../dossiers/dossiers.module";
+import { JobLogModule } from "../job-log/job-log.module";
 
 @Module({
   imports: [
     //TODO: Logger module should be global (see @Global at nestjs docs)
     LoggerModule,
-
     DossiersModule,
+    JobLogModule
   ],
   controllers: [],
   providers: [CronService],
