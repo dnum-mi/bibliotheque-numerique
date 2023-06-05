97db4318b539724b2a3fb1af1c5e9c9903e618f1
Robin <pulsarr@Pulsarr-MacbookPro-M2.local>
2023-06-05 09:50:41 +0200

    fixing injection error on worker

--- apps/server/src/modules/cron/cron.module.ts
+++ apps/server/src/modules/cron/cron.module.ts
@@ -3,12 +3,14 @@ import { CronService } from "./cron.service";
 import { LoggerModule } from "../logger/logger.module";
 import { DossiersModule } from "../dossiers/dossiers.module";
 import { JobLogModule } from "../job-log/job-log.module";
+import { DemarchesModule } from "../demarches/demarches.module";
 
 @Module({
   imports: [
     //TODO: Logger module should be global (see @Global at nestjs docs)
     LoggerModule,
     DossiersModule,
+    DemarchesModule,
     JobLogModule,
   ],
   controllers: [],
