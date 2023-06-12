b935e178e04e62b0f050e3810d4096b7b06568de
baudointran <baudointran@users.noreply.github.com>
2023-06-12 16:16:09 +0200

    fix: rename to job-log

--- apps/server/src/modules/job-log/job-log.module.ts
+++ apps/server/src/modules/job-log/job-log.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { JobLog } from "./objects/job-log.entity";
-import { JobLogController } from "./controllers/job-lob.controller";
+import { JobLogController } from "./controllers/job-log.controller";
 import { JobLogService } from "./providers/job-log.service";
 
 @Module({
