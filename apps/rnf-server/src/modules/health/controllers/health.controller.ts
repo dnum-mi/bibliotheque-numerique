a8df476a8772d7b3569070526d469ac40765b180
Robin <pulsarr@Pulsarr-MacbookPro-M2.local>
2023-06-16 16:24:43 +0200

    using beta version of terminus for prisma support

--- apps/rnf-server/src/modules/health/controllers/health.controller.ts
+++ apps/rnf-server/src/modules/health/controllers/health.controller.ts
@@ -1,15 +1,18 @@
 import { Controller, Get } from "@nestjs/common";
-import { HealthCheck, HealthCheckService, HttpHealthIndicator, TypeOrmHealthIndicator } from "@nestjs/terminus";
+import { HealthCheck, HealthCheckService, PrismaHealthIndicator } from "@nestjs/terminus";
 import { ApiTags } from "@nestjs/swagger";
+import { PrismaService } from "@/shared/modules/prisma/providers/prisma.service";
 
 @ApiTags("Health")
 @Controller("health")
 export class HealthController {
-  constructor(private health: HealthCheckService, private http: HttpHealthIndicator, private db: TypeOrmHealthIndicator) {}
+  constructor(private health: HealthCheckService, private readonly prisma: PrismaHealthIndicator, private prismaService: PrismaService) {}
 
   @Get()
   @HealthCheck()
   check() {
-    return this.health.check([() => this.db.pingCheck("database")]);
+    // TODO: ask Terminus module to code with TYPE.
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return
+    return this.health.check([() => this.prisma.pingCheck("prisma", this.prismaService)]);
   }
 }
