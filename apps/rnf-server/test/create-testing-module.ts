ffd351f803f1e45aedc88136a80d02c4a09ccaf9
Robin <dev@pulsarr.fr>
2023-07-05 11:39:59 +0200

    no relative path for test file

--- apps/rnf-server/test/create-testing-module.ts
+++ apps/rnf-server/test/create-testing-module.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppModule } from "@/app.module";
 import { DsService } from "@/modules/ds/providers/ds.service";
-import { dsServiceMock } from "@/test/mocks/ds-service.mock";
 import { LoggerService } from "@/shared/modules/logger/providers/logger.service";
-import { loggerServiceMock } from "@/test/mocks/logger-service.mock";
 import { mainConfig } from "@/main.config";
 import { PrismaService } from "@/shared/modules/prisma/providers/prisma.service";
 import { INestApplication } from "@nestjs/common";
+import { dsServiceMock } from "./mocks/ds-service.mock";
+import { loggerServiceMock } from "./mocks/logger-service.mock";
 
 export const createTestingModule = async (): Promise<{
   app: INestApplication;
