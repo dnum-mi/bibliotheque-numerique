af1992e14b3cd39643623c18ce80423af48bddbc
baudointran <baudointran@users.noreply.github.com>
2023-07-26 16:25:26 +0200

    feat: link to reset passord in profile and fix test component

--- apps/client/src/views/Profile.cy.ts
+++ apps/client/src/views/Profile.cy.ts
@@ -1,33 +1,27 @@
-import '@gouvminint/vue-dsfr/styles'
-import '../main.css'
-
-import VueDsfr from '@gouvminint/vue-dsfr'
-
-import { createPinia } from 'pinia'
 import { useUserStore } from '@/stores'
 import { createRandomAdmin } from './__tests__/users'
 
 import Profile from './Profile.vue'
+import { createPinia } from 'pinia'
+import { ASK_RESET_PWD_SUCCESS } from '../messages'
 
 describe('<Profile />', () => {
   it('renders', () => {
+    cy.intercept('/api/users/reset-password', { success: true })
+
     const pinia = createPinia()
     const useStore = useUserStore(pinia)
     const newUser = createRandomAdmin()
     useStore.currentUser = newUser
     const userRols = newUser.roles
-    const extensions = {
-      use: [
-        pinia,
-        VueDsfr,
-      ],
-    }
-    cy.mount(Profile, {
-      extensions,
-    })
+
+    cy.mount(Profile, { stores: pinia })
     cy.contains(newUser.email)
     cy.contains('La liste des vos rôles')
     cy.contains(userRols[0].name)
     cy.contains(userRols[0].description)
+
+    cy.contains('Modifier mon mot de passe').click()
+    cy.get('.fr-alert').should('contain', ASK_RESET_PWD_SUCCESS)
   })
 })
