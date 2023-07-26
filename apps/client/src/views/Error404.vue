bd55d1d40c9a466275456a19b1d7803554e8c58a
Stanislas Ormières <stan@stormier.ninja>
2023-07-26 10:00:06 +0200

    fix(Error404): :bug: use router-link instead of anchor

--- apps/client/src/views/Error404.vue
+++ apps/client/src/views/Error404.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="fr-my-7w fr-mt-md-12w fr-mb-md-10w fr-grid-row fr-grid-row--gutters fr-grid-row--middle fr-grid-row--center">
-    <div class="fr-py-0 fr-col-12 fr-col-md-6">
+  <div class="fr-my-7w  fr-mt-md-12w  fr-mb-md-10w  fr-grid-row  fr-grid-row--gutters  fr-grid-row--middle  fr-grid-row--center">
+    <div class="fr-py-0  fr-col-12  fr-col-md-6">
       <h1>Page non trouvée</h1>
-      <p class="fr-text--sm fr-mb-3w">
+      <p class="fr-text--sm  fr-mb-3w">
         Erreur 404
       </p>
-      <p class="fr-text--lead fr-mb-3w">
+      <p class="fr-text--lead  fr-mb-3w">
         La page que vous cherchez est introuvable. Excusez-nous pour la gène occasionnée.
       </p>
-      <ul class="fr-btns-group fr-btns-group--inline-md">
+      <ul class="fr-btns-group  fr-btns-group--inline-md">
         <li>
-          <a
+          <RouterLink
             class="fr-btn"
-            href="/"
+            to="/"
           >
             Page d'accueil
-          </a>
+          </RouterLink>
         </li>
       </ul>
     </div>
