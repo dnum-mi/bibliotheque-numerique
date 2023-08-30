ff179589ae5dd5dd3e0197340bfaf1f5b97144a0
pengfei.dong <mail@peng-fei.name>
2023-08-30 09:33:15 +0200

    debug for build and deploye

--- apps/rnf-server/src/modules/ds/objects/mappers/universal.mapper.ts
+++ apps/rnf-server/src/modules/ds/objects/mappers/universal.mapper.ts
@@ -1,5 +1,5 @@
 import { Champ } from '@dnum-mi/ds-api-client'
-import { AddressChamp } from '@dnum-mi/ds-api-client/dist/@types/types'
+// import { AddressChamp } from '@dnum-mi/ds-api-client/dist/@types/types'
 import { Mapper } from '@/modules/ds/objects/types/mapper.type'
 
 const stringValue = (ch: Champ | null) => ch?.stringValue ?? null
@@ -9,25 +9,27 @@ export const universalMapper: Mapper = {
   email: stringValue,
   phone: stringValue,
   type: stringValue,
-  address: (ch: AddressChamp | null) => {
-    if (!ch || ch.__typename !== 'AddressChamp' || !ch.address) {
-      return null
-    }
-    const address = ch.address
-    return {
-      label: address.label,
-      type: address.type,
-      streetAddress: address.streetAddress ?? null,
-      streetNumber: address.streetNumber ?? null,
-      streetName: address.streetName ?? null,
-      postalCode: address.postalCode,
-      cityName: address.cityName,
-      cityCode: address.cityCode,
-      departmentName: address.departmentName ?? null,
-      departmentCode: address.departmentCode ?? null,
-      regionName: address.regionName ?? null,
-      regionCode: address.regionCode ?? null,
-    }
-  },
+  address: stringValue,
+  // TODO : debug address mapping
+  // address: (ch: AddressChamp | null) => {
+  //   if (!ch || ch.__typename !== 'AddressChamp' || !ch.address) {
+  //     return null
+  //   }
+  //   const address = ch.address
+  //   return {
+  //     label: address.label,
+  //     type: address.type,
+  //     streetAddress: address.streetAddress ?? null,
+  //     streetNumber: address.streetNumber ?? null,
+  //     streetName: address.streetName ?? null,
+  //     postalCode: address.postalCode,
+  //     cityName: address.cityName,
+  //     cityCode: address.cityCode,
+  //     departmentName: address.departmentName ?? null,
+  //     departmentCode: address.departmentCode ?? null,
+  //     regionName: address.regionName ?? null,
+  //     regionCode: address.regionCode ?? null,
+  //   }
+  // },
   personInFoundationToCreate: (ch: Champ) => null,
 }
