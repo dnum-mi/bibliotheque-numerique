76da574c83b4d1a53fff0099a22013ac9d96ce3a
baudointran <baudointran@users.noreply.github.com>
2023-09-04 15:40:04 +0200

    fix: create rnf database

--- apps/server/docker/docker-entrypoint-initdb.d/init.sql
+++ apps/server/docker/docker-entrypoint-initdb.d/init.sql
@@ -2,5 +2,5 @@ CREATE DATABASE "keycloak";
 CREATE DATABASE "biblio-num-unit";
 CREATE DATABASE "biblio-num-e2e";
 CREATE DATABASE "rnf";
-CREATE DATABASE "rnf-unit";
-CREATE DATABASE "rnf-e2e";
+CREATE DATABASE "rnf-testing";
+
