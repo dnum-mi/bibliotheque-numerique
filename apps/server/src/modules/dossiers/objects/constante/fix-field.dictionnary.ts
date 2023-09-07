ad62ed83df3eb7bb6e05f3792a7611dd08cf40e2
baudointran <baudointran@users.noreply.github.com>
2023-09-07 13:48:46 +0200

    feat: add the dates of dossiers infix-fields: date de depot, date d\'instruction, etc

--- apps/server/src/modules/dossiers/objects/constante/fix-field.dictionnary.ts
+++ apps/server/src/modules/dossiers/objects/constante/fix-field.dictionnary.ts
@@ -12,6 +12,27 @@ export const fixFields: MappingColumn[] = [
     source: FieldSource.fixField,
   },
   {
+    id: '74dff521-5292-485c-b34d-0a97420328d9',
+    columnLabel: 'Date de dépot',
+    originalLabel: 'Date de dépot',
+    type: FieldType.date,
+    source: FieldSource.fixField,
+  },
+  {
+    id: 'c3046f1a-ff31-4b93-a1d3-3b128ae6e51f',
+    columnLabel: 'Date de passage en instruction',
+    originalLabel: 'Date de passage en instruction',
+    type: FieldType.date,
+    source: FieldSource.fixField,
+  },
+  {
+    id: '91ad7ec7-bf4d-4294-bd0b-c1bca93f4b33',
+    columnLabel: 'Date de passage en construction',
+    originalLabel: 'Date de passage en construction',
+    type: FieldType.date,
+    source: FieldSource.fixField,
+  },
+  {
     id: '1a4b62c4-b81f-4e83-ac34-f6d601b8a8d4',
     columnLabel: 'Status',
     originalLabel: 'state',
@@ -44,6 +65,7 @@ export const fixFields: MappingColumn[] = [
 
 export const fixFieldValueFunctions: Record<string, FixFieldValueGetter> = {
   '96151176-4624-4706-b861-722d2e53545d': (dossier) => dossier.number,
+  '74dff521-5292-485c-b34d-0a97420328d9': (dossier) => dossier.dateDepot,
   '1a4b62c4-b81f-4e83-ac34-f6d601b8a8d4': (dossier) => dossier.state,
   '9863ce70-6378-4d7e-aca9-b81fb7b97c11': (dossier) => dossier.dateDepot,
   '9863ce70-6378-4d7e-aca9-b81fb7b97c12': (dossier) => dossier.datePassageEnInstruction,
