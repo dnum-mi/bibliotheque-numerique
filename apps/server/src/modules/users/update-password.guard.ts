4763942ab210a9c5579dac93a545967ed837e0b2
baudointran <16351036+baudointran@users.noreply.github.com>
2023-10-13 15:16:43 +0200

    fix: verify jwt (#811)
    Co-authored-by: baudointran <baudointran@users.noreply.github.com>

--- apps/server/src/modules/users/update-password.guard.ts
+++ apps/server/src/modules/users/update-password.guard.ts
@@ -3,10 +3,16 @@ import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from
 import { JwtService } from '@nestjs/jwt'
 import { Observable } from 'rxjs'
 import { UsersService } from './users.service'
+import { LoggerService } from '@/shared/modules/logger/logger.service'
 
 @Injectable()
 export class UpdatePasswordGuard implements CanActivate {
-  constructor (private readonly jwtService: JwtService, private readonly userService: UsersService) {}
+  constructor (private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+    protected logger: LoggerService,
+  ) {
+    this.logger.setContext(this.constructor.name)
+  }
 
   canActivate (context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest()
@@ -19,13 +25,18 @@ export class UpdatePasswordGuard implements CanActivate {
 
     const { token } = body
     if (!token) throw new UnauthorizedException()
-    // jwt does not go natively in url. We had to transform it in base64
-    const tokenJwt = Buffer.from(token, 'base64url').toString()
-    const decode = this.jwtService.decode(tokenJwt)
-    if (!decode || typeof decode === 'string') return false
-    const user = await this.userService.findOneById(decode.user)
-    if (!user) return false
-    request.user = user
-    return true
+    try {
+      // jwt does not go natively in url. We had to transform it in base64
+      const tokenJwt = Buffer.from(token, 'base64url').toString()
+      const decode = this.jwtService.verify(tokenJwt)
+      if (!decode || typeof decode === 'string') return false
+      const user = await this.userService.findOneById(decode.user)
+      if (!user) return false
+      request.user = user
+      return true
+    } catch (error) {
+      this.logger.error(error)
+    }
+    return false
   }
 }
--- apps/server/src/modules/users/validate-sign-up.guard.ts
+++ apps/server/src/modules/users/validate-sign-up.guard.ts
@@ -3,14 +3,19 @@ import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from
 import { JwtService } from '@nestjs/jwt'
 import { Observable } from 'rxjs'
 import { UsersService } from './users.service'
+import { LoggerService } from '@/shared/modules/logger/logger.service'
 
 @Injectable()
 export class ValidSignUpGuard implements CanActivate {
-  constructor (private readonly jwtService: JwtService, private readonly userService: UsersService) {}
+  constructor (private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+    protected logger: LoggerService,
+  ) {
+    this.logger.setContext(this.constructor.name)
+  }
 
   canActivate (context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest()
-
     return this.validateRequest(request)
   }
 
@@ -21,11 +26,16 @@ export class ValidSignUpGuard implements CanActivate {
     if (!token) throw new UnauthorizedException()
     // jwt does not go natively in url. We had to transform it in base64
     const tokenJwt = Buffer.from(token, 'base64url').toString()
-    const decode = this.jwtService.decode(tokenJwt)
-    if (!decode || typeof decode === 'string') return false
-    const user = await this.userService.findOneOrThrow({ where: { email: decode.user } })
-    if (!user) return false
-    request.user = user
-    return true
+    try {
+      const decode = this.jwtService.verify(tokenJwt)
+      if (!decode || typeof decode === 'string') return false
+      const user = await this.userService.findOneOrThrow({ where: { email: decode.user } })
+      if (!user) return false
+      request.user = user
+      return true
+    } catch (error) {
+      this.logger.error(error)
+    }
+    return false
   }
 }
