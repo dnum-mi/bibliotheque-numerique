285ccdc86320dd86c750bf0cb778f9ff84219286
baudointran <16351036+baudointran@users.noreply.github.com>
2023-10-31 18:27:22 +0100

    feat: update roles utils on server (#878)
    Co-authored-by: baudointran <baudointran@users.noreply.github.com>

--- packages/shared/src/enums/roles/roles.enum.ts
+++ packages/shared/src/enums/roles/roles.enum.ts
@@ -6,12 +6,3 @@ export const Roles = {
 }
 
 export type RolesKeys = (typeof Roles)[keyof typeof Roles]
-
-export const RoleHierarchy: { [key in RolesKeys]: number } = {
-  sudo: 0,
-  superadmin: 1,
-  admin: 2,
-  instructor: 3,
-  any: 10,
-  undefined: 11,
-}
