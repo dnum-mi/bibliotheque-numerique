f5cf9475362090eccf316184adc09b6157432f1b
Pulsarr <dev@pulsarr.fr>
2023-10-31 18:53:56 +0100

    enhancing role utils (#880)

--- packages/shared/src/utils/role.utils.ts
+++ packages/shared/src/utils/role.utils.ts
@@ -1,5 +1,5 @@
 import { Roles, RolesKeys } from '../enums'
-import { IRole, IRoleOption } from '../interfaces'
+import { IRole } from '../interfaces'
 
 export const RoleHierarchy: { [key in RolesKeys]: number } = {
   sudo: 0,
@@ -8,15 +8,21 @@ export const RoleHierarchy: { [key in RolesKeys]: number } = {
   instructor: 3,
   any: 10,
   undefined: 11,
+  null: 11,
 }
 
-export const canAccessDemarche = (id: number, role:IRole): boolean => role.options.some((option: IRoleOption) => option.idDemarche === id)
+export const canAccessDemarche = (id: number, role:IRole): boolean =>
+  isSuperiorOrSimilar(Roles.superadmin, role.label) || !!role.options[id]
 
-export const canAccessPrefecture = (pref: string, role: IRole): boolean => role.options.some((option: IRoleOption) => option.national || option.prefectures.some(prefecture => prefecture === pref))
+// TODO: should not be string, should be PrefectureKeys
+export const canAccessPrefectureInDemarche = (prefecture: string, role: IRole, demarcheId: number): boolean =>
+  isSuperiorOrSimilar(Roles.superadmin, role.label) ||
+  role.options[demarcheId]?.national ||
+  role.options[demarcheId]?.prefectures?.includes(prefecture)
 
 export const isSuperiorOrSimilar = (
   comparedTo: RolesKeys,
-  toCompare: RolesKeys,
-): boolean => RoleHierarchy[toCompare] <= RoleHierarchy[comparedTo]
+  toCompare: RolesKeys | null,
+): boolean => RoleHierarchy['' + toCompare] <= RoleHierarchy[comparedTo]
 
 export const isAtLeastAdmin = (key: RolesKeys) => isSuperiorOrSimilar(Roles.admin, key)
