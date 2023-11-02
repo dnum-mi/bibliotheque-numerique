07f51a115392020201a87dd9adca9836bae95fcd
Robin <dev@pulsarr.fr>
2023-11-02 12:39:15 +0100

    adding some coverage on backend

--- apps/server/test/unit/launch-unit-test.sh
+++ apps/server/test/unit/launch-unit-test.sh
@@ -5,9 +5,10 @@ export NODE_ENV='test-unit'
 # This script is used to launch the e2e tests.
 clear
 echo "============================================\n======== Launching unit tests.. ============\n============================================"
-if [ "x$1" = "xdev" ]
-then
+if [ "x$1" = "xdev" ]; then
     dotenv -e ./test/.env.test jest -- --config ./test/unit/jest-unit.json --watch --silent=false
+elif [ "x$1" = "xcoverage" ]; then
+    dotenv -e ./test/.env.test jest -- --config ./test/unit/jest-unit.json --silent=true --coverage=true
 else
-    dotenv -e ./test/.env.test jest -- --config ./test/unit/jest-unit.json --silent=true --detectOpenHandles --coverage=true
+    dotenv -e ./test/.env.test jest -- --config ./test/unit/jest-unit.json --silent=true --detectOpenHandles
 fi
