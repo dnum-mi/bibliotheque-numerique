cb19abd21ab5a58d516b8567cb40b494a24d8a76
Robin <dev@pulsarr.fr>
2023-11-03 07:36:37 +0100

    Better management of validation error details for console and client

---
+++ apps/server/src/shared/exceptions/filters/custom-validation-exception.filter.ts
@@ -0,0 +1,33 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common'
+import { HttpAdapterHost } from '@nestjs/core'
+import { LoggerService } from '../../modules/logger/logger.service'
+import { CustomValidationException } from '@/shared/exceptions/custom-validation.exception'
+
+@Catch(CustomValidationException)
+export class CustomValidationExceptionFilter implements ExceptionFilter {
+  constructor (private readonly httpAdapterHost: HttpAdapterHost, private readonly logger: LoggerService) {
+    this.logger.setContext(this.constructor.name)
+  }
+
+  catch (exception: CustomValidationException, host: ArgumentsHost): void {
+    this.logger.verbose('catch')
+    const { httpAdapter } = this.httpAdapterHost
+
+    const ctx = host.switchToHttp()
+    this.logger.warn('400 BadRequest Exception: Validation failed')
+    this.logger.debug(exception.validationErrors)
+
+    httpAdapter.reply(
+      ctx.getResponse(),
+      {
+        statusCode: 400,
+        message: 'Validation failed',
+        data: {
+          errors: exception.validationErrors,
+        },
+        path: httpAdapter.getRequestUrl(ctx.getRequest()) as string,
+      },
+      424,
+    )
+  }
+}
