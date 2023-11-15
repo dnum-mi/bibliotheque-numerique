48784be1755fe1b1533fa30bf8cb8e927efc533b
Robin <dev@pulsarr.fr>
2023-11-15 10:37:37 +0100

    generating user with editable role and unit testing it

---
+++ apps/server/src/modules/users/utils/role.utils.spec.ts
@@ -0,0 +1,480 @@
+import {
+  IRole,
+  IUser,
+  OrganismeType,
+  Prefecture,
+  Roles,
+  SmallDemarcheOutputDto,
+} from '@biblio-num/shared'
+import { generateUserWithEditableRole } from '@/modules/users/utils/role.utils'
+
+/* region MOCK DEMARCHES */
+/*
+   CULTE
+      Démarche 1
+      Démarche 2
+   FDD
+      Démarche 3
+      Démarche 4
+   FE
+      Démarche 4
+   AUCUN
+      Démarche 5
+ */
+const allSmallDemarches: SmallDemarcheOutputDto[] = [
+  {
+    id: 1,
+    dsId: 1,
+    types: [OrganismeType.CULTE],
+    title: 'Démarche 1',
+  },
+  {
+    id: 2,
+    dsId: 2,
+    types: [OrganismeType.CULTE],
+    title: 'Démarche 2',
+  },
+  {
+    id: 3,
+    dsId: 3,
+    types: [OrganismeType.FDD],
+    title: 'Démarche 3',
+  },
+  {
+    id: 4,
+    dsId: 4,
+    types: [OrganismeType.FDD, OrganismeType.FE],
+    title: 'Démarche 4',
+  },
+  {
+    id: 5,
+    dsId: 5,
+    types: [],
+    title: 'Démarche 5',
+  },
+]
+/* endregion */
+
+/* region MOCK USER */
+let useless = 100
+
+const dumbUserFromRole = (role: IRole): IUser => ({
+  id: useless++,
+  email: `${useless}@test-unit.fr`,
+  job: 'useless',
+  lastname: 'useless',
+  firstname: 'useless',
+  validated: true,
+  password: 'useless',
+  updateAt: new Date(),
+  createAt: new Date(),
+  role,
+})
+
+const superAdmin: IUser = dumbUserFromRole({
+  label: Roles.superadmin,
+  options: {},
+})
+const emptyAdmin: IUser = dumbUserFromRole({
+  label: Roles.admin,
+  options: {},
+})
+const emptyInstructor: IUser = dumbUserFromRole({
+  label: Roles.instructor,
+  options: {},
+})
+
+/* endregion */
+
+describe('RoleUtils', () => {
+  describe('generateUserWithEditableRole', () => {
+    it('Should throw an error if editor its not an admin', () => {
+      expect(() => {
+        generateUserWithEditableRole(
+          emptyInstructor,
+          emptyInstructor,
+          allSmallDemarches,
+        )
+      }).toThrow('Only admins can generate user for admin output')
+    })
+
+    it('Should return nothing editable if target is superadmin', () => {
+      expect(
+        generateUserWithEditableRole(
+          superAdmin,
+          superAdmin,
+          allSmallDemarches,
+        ),
+      ).toEqual({
+        originalUser: superAdmin,
+        demarcheHash: Object.fromEntries(
+          allSmallDemarches.map((sd, i) => [
+            sd.id,
+            {
+              ...allSmallDemarches[i],
+              checked: false,
+              editable: false,
+              prefectureOptions: {
+                national: {
+                  value: false,
+                  editable: false,
+                },
+                prefectures: {
+                  value: [],
+                  deletable: [],
+                  addable: [],
+                },
+              },
+            },
+          ]),
+        ),
+      })
+    })
+
+    it('Should return everything editable if editor is superadmin and target is instructor', () => {
+      expect(
+        generateUserWithEditableRole(
+          superAdmin,
+          emptyInstructor,
+          allSmallDemarches,
+        ),
+      ).toEqual({
+        originalUser: emptyInstructor,
+        demarcheHash: Object.fromEntries(
+          allSmallDemarches.map((sd, i) => [
+            sd.id,
+            {
+              ...allSmallDemarches[i],
+              checked: false,
+              editable: true,
+              prefectureOptions: {
+                national: {
+                  value: false,
+                  editable: true,
+                },
+                prefectures: {
+                  value: [],
+                  deletable: [],
+                  addable: [],
+                },
+              },
+            },
+          ]),
+        ),
+      })
+    })
+
+    it('Should return everything editable if editor is superadmin and target is admin', () => {
+      expect(
+        generateUserWithEditableRole(
+          superAdmin,
+          emptyAdmin,
+          allSmallDemarches,
+        ),
+      ).toEqual({
+        originalUser: emptyAdmin,
+        demarcheHash: Object.fromEntries(
+          allSmallDemarches.map((sd, i) => [
+            sd.id,
+            {
+              ...allSmallDemarches[i],
+              checked: false,
+              editable: true,
+              prefectureOptions: {
+                national: {
+                  value: false,
+                  editable: true,
+                },
+                prefectures: {
+                  value: [],
+                  deletable: [],
+                  addable: [],
+                },
+              },
+            },
+          ]),
+        ),
+      })
+    })
+
+    it('Should return everything editable and keep value of initial user', () => {
+      const bobInstructor = dumbUserFromRole({
+        label: Roles.instructor,
+        options: {
+          1: {
+            national: true,
+            prefectures: [],
+          },
+          3: {
+            national: false,
+            prefectures: [Prefecture.D57, Prefecture.D75],
+          },
+        },
+      })
+      expect(
+        generateUserWithEditableRole(
+          superAdmin,
+          bobInstructor,
+          allSmallDemarches,
+        ),
+      ).toEqual({
+        originalUser: bobInstructor,
+        demarcheHash: Object.fromEntries(
+          allSmallDemarches.map((sd, i) => [
+            sd.id,
+            {
+              ...allSmallDemarches[i],
+              editable: true,
+              checked: bobInstructor.role.options[sd.id] !== undefined,
+              prefectureOptions: {
+                national: {
+                  value: bobInstructor.role.options[sd.id]?.national ?? false,
+                  editable: true,
+                },
+                prefectures: {
+                  value: bobInstructor.role.options[sd.id]?.prefectures ?? [],
+                  deletable: [],
+                  addable: [],
+                },
+              },
+            },
+          ]),
+        ),
+      })
+    })
+
+    it('National Admin should be able to add national empty user', () => {
+      const customAdmin = dumbUserFromRole({
+        label: Roles.admin,
+        options: {
+          1: {
+            national: true,
+            prefectures: [],
+          },
+        },
+      })
+      const result = generateUserWithEditableRole(
+        customAdmin,
+        emptyInstructor,
+        allSmallDemarches,
+      )
+      expect(result.demarcheHash[1]).toEqual({
+        ...allSmallDemarches[0],
+        checked: false,
+        editable: true,
+        prefectureOptions: {
+          national: {
+            value: false,
+            editable: true,
+          },
+          prefectures: {
+            value: [],
+            deletable: [],
+            addable: [],
+          },
+        },
+      })
+    })
+
+    it('National Admin should be able to unchecked demarche', () => {
+      const customAdmin = dumbUserFromRole({
+        label: Roles.admin,
+        options: {
+          1: {
+            national: true,
+            prefectures: [],
+          },
+        },
+      })
+      const bobInstructor = dumbUserFromRole({
+        label: Roles.instructor,
+        options: {
+          1: {
+            national: false,
+            prefectures: [Prefecture.D57],
+          },
+        },
+      })
+      const result = generateUserWithEditableRole(
+        customAdmin,
+        bobInstructor,
+        allSmallDemarches,
+      )
+      expect(result.demarcheHash[1]).toEqual({
+        ...allSmallDemarches[0],
+        checked: true,
+        editable: true,
+        prefectureOptions: {
+          national: {
+            value: false,
+            editable: true,
+          },
+          prefectures: {
+            value: [Prefecture.D57],
+            deletable: [],
+            addable: [],
+          },
+        },
+      })
+    })
+
+    it('Prefecture Admin should be able to add its own prefecture to empty user', () => {
+      const customAdmin = dumbUserFromRole({
+        label: Roles.admin,
+        options: {
+          1: {
+            national: false,
+            prefectures: [Prefecture.D57, Prefecture.D75],
+          },
+        },
+      })
+      const result = generateUserWithEditableRole(
+        customAdmin,
+        emptyInstructor,
+        allSmallDemarches,
+      )
+      expect(result.demarcheHash[1]).toEqual({
+        ...allSmallDemarches[0],
+        checked: false,
+        editable: true,
+        prefectureOptions: {
+          national: {
+            value: false,
+            editable: false,
+          },
+          prefectures: {
+            value: [],
+            deletable: [],
+            addable: [Prefecture.D57, Prefecture.D75],
+          },
+        },
+      })
+    })
+
+    it('Prefecture Admin should be able to delete its own prefecture to empty user', () => {
+      const customAdmin = dumbUserFromRole({
+        label: Roles.admin,
+        options: {
+          1: {
+            national: false,
+            prefectures: [Prefecture.D57, Prefecture.D75],
+          },
+        },
+      })
+      const bobInstructor = dumbUserFromRole({
+        label: Roles.instructor,
+        options: {
+          1: {
+            national: false,
+            prefectures: [Prefecture.D57, Prefecture.D75],
+          },
+        },
+      })
+      const result = generateUserWithEditableRole(
+        customAdmin,
+        bobInstructor,
+        allSmallDemarches,
+      )
+      expect(result.demarcheHash[1]).toEqual({
+        ...allSmallDemarches[0],
+        checked: true,
+        editable: true,
+        prefectureOptions: {
+          national: {
+            value: false,
+            editable: false,
+          },
+          prefectures: {
+            value: [Prefecture.D57, Prefecture.D75],
+            deletable: [Prefecture.D57, Prefecture.D75],
+            addable: [],
+          },
+        },
+      })
+    })
+
+    it('Prefecture Admin should not be able to edit national Instructor', () => {
+      const customAdmin = dumbUserFromRole({
+        label: Roles.admin,
+        options: {
+          1: {
+            national: false,
+            prefectures: [Prefecture.D57, Prefecture.D75],
+          },
+        },
+      })
+      const bobInstructor = dumbUserFromRole({
+        label: Roles.instructor,
+        options: {
+          1: {
+            national: true,
+            prefectures: [],
+          },
+        },
+      })
+      const result = generateUserWithEditableRole(
+        customAdmin,
+        bobInstructor,
+        allSmallDemarches,
+      )
+      expect(result.demarcheHash[1]).toEqual({
+        ...allSmallDemarches[0],
+        checked: true,
+        editable: false,
+        prefectureOptions: {
+          national: {
+            value: true,
+            editable: false,
+          },
+          prefectures: {
+            value: [],
+            deletable: [],
+            addable: [],
+          },
+        },
+      })
+    })
+
+    it('Prefecture Admin should not be able to uncheck wider prefecture Instructor', () => {
+      const customAdmin = dumbUserFromRole({
+        label: Roles.admin,
+        options: {
+          1: {
+            national: false,
+            prefectures: [Prefecture.D57, Prefecture.D75],
+          },
+        },
+      })
+      const bobInstructor = dumbUserFromRole({
+        label: Roles.instructor,
+        options: {
+          1: {
+            national: false,
+            prefectures: [Prefecture.D57, Prefecture.D30],
+          },
+        },
+      })
+      const result = generateUserWithEditableRole(
+        customAdmin,
+        bobInstructor,
+        allSmallDemarches,
+      )
+      expect(result.demarcheHash[1]).toEqual({
+        ...allSmallDemarches[0],
+        checked: true,
+        editable: false,
+        prefectureOptions: {
+          national: {
+            value: false,
+            editable: false,
+          },
+          prefectures: {
+            value: [Prefecture.D57, Prefecture.D30],
+            deletable: [Prefecture.D57],
+            addable: [Prefecture.D75],
+          },
+        },
+      })
+    })
+  })
+})
