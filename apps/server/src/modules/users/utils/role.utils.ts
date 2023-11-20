7f0db9bdd6096fc308e3c90addb0e82e1cd75871
Robin <dev@pulsarr.fr>
2023-11-20 09:38:42 +0100

    sudo can edit everything too

--- apps/server/src/modules/users/utils/role.utils.ts
+++ apps/server/src/modules/users/utils/role.utils.ts
@@ -64,7 +64,7 @@ export const generateUserWithEditableRole = (
     ]),
   )
   if (isBelowSuperAdmin(target.role.label)) {
-    if (editor.role.label === Roles.superadmin) {
+    if (isSuperiorOrSimilar(Roles.superadmin, editor.role.label)) {
       Object.values(hash).forEach((value) => {
         value.editable = true
         value.prefectureOptions.national.editable = true
