61fa34390704ec16417498d4d6bb2d13cac5ddca
baudointran <baudointran@users.noreply.github.com>
2023-11-24 11:58:52 +0100

    feat(Configuration): add components and views for configuration

---
+++ apps/client/src/views/Configuration/Configuration.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LayoutList from '@/components/Layout/LayoutList.vue'
+
+</script>
+<template>
+  <LayoutList
+    title="Configuration"
+    title-bg-color="var(--warning-425-625)"
+    title-icon="fr-icon-settings-5-line"
+  />
+</template>
