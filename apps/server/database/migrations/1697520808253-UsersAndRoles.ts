1db45f123f19d4d8c14c7cabb6ca53db1f34b818
Robin <dev@pulsarr.fr>
2023-11-29 15:45:11 +0100

    fix: old migration pb

--- apps/server/database/migrations/1697520808253-UsersAndRoles.ts
+++ apps/server/database/migrations/1697520808253-UsersAndRoles.ts
@@ -5,7 +5,7 @@ export class UsersAndRoles1697520808253 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     const adminRoleId = await queryRunner.query('SELECT "id" FROM roles WHERE name = $1', ['admin'])
-    const usersId = await queryRunner.query('SELECT "usersId" FROM users_roles WHERE "rolesId" = $1', [adminRoleId[0].id])
+    const usersId = await queryRunner.query('SELECT "usersId" FROM users_roles WHERE "rolesId" = $1', [adminRoleId?.[0]?.id])
     await queryRunner.dropTable('users_roles')
     await queryRunner.dropTable('roles')
     await queryRunner.query(
@@ -17,8 +17,10 @@ export class UsersAndRoles1697520808253 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "users" ADD "role" jsonb NOT NULL DEFAULT '{"label":null,"options":[]}'`,
     )
-    await queryRunner.query(`UPDATE "users" SET "role" = $1 WHERE "id" = ANY($2)`,
-      [JSON.stringify({label: 'sudo', options:[]}), usersId.map(u => u.usersId)])
+    if (usersId?.length) {
+      await queryRunner.query(`UPDATE "users" SET "role" = $1 WHERE "id" = ANY($2)`,
+        [JSON.stringify({label: 'sudo', options:[]}), usersId.map(u => u.usersId)])
+    }
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
