72d9b209455632225bd855647c9be4fc8ff7161d
Robin <dev@pulsarr.fr>
2023-12-08 08:20:00 +0100

    chore/logging in json mode for kibana

--- apps/server/src/shared/modules/logger/logger.module.ts
+++ apps/server/src/shared/modules/logger/logger.module.ts
@@ -1,8 +1,6 @@
-import { Global, MiddlewareConsumer, Module, Scope } from '@nestjs/common'
+import { Global, Module, Scope } from '@nestjs/common'
 import { LoggerService } from './logger.service'
 import { ConfigService } from '@nestjs/config'
-import { MorganMiddleware } from '@nest-middlewares/morgan'
-import { jsonFormat } from './morgan-jsonformat.function'
 
 @Global()
 @Module({
@@ -19,22 +17,4 @@ import { jsonFormat } from './morgan-jsonformat.function'
   ],
   exports: [LoggerService],
 })
-export class LoggerModule {
-  constructor (private readonly config: ConfigService, private readonly loggerService: LoggerService) {
-    this.loggerService.setContext(this.constructor.name)
-  }
-
-  configure (consumer: MiddlewareConsumer): void {
-    this.config.get('isDev')
-      ? MorganMiddleware.configure(':method :url :status    (:response-time ms)', {
-        stream: {
-          // delete last two characters (line break and carriage return)
-          write: (message) => this.loggerService.log(message.substring(0, message.length - 1)),
-        },
-      })
-      : MorganMiddleware.configure(jsonFormat)
-    if (!this.config.get('isTest')) {
-      consumer.apply(MorganMiddleware).forRoutes('*')
-    }
-  }
-}
+export class LoggerModule {}
