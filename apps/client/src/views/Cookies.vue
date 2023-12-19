67bca6e5a86f12b698037ecd7f6547b878342d49
pengfei.dong <mail@peng-fei.name>
2023-12-19 16:45:27 +0100

    fix: add page cookies for client

---
+++ apps/client/src/views/Cookies.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="fr-container">
+    <div class="fr-my-7w fr-mb-md-10w fr-grid-row fr-grid-row--gutters fr-grid-row--middle fr-grid-row--center">
+      <div class="fr-py-0 fr-col-12 fr-col-md-6">
+        <h1>Cookies déposés et configuration du suivi </h1>
+        <p class="fr-text--lead fr-mb-3w">
+          Ce site dépose un petit fichier texte (un « cookie ») sur votre ordinateur lorsque vous le consultez. Cela nous permet de mesurer le nombre de visites et de comprendre quelles sont les pages les plus consultées.
+        </p>
+        <h1>Comment désactiver le suivi statistique sur mon navigateur ?</h1>
+        <p class="fr-text--lead fr-mb-3w">
+          Si vous souhaitez désactiver ce suivi statistique, il vous suffit d’activer la fonctionnalité « Ne pas me pister » de votre navigateur. Notre outil de suivi le prendra en compte, et cessera d’inclure vos visites dans les statistiques.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
