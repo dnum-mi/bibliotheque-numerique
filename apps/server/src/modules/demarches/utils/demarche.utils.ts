b00a600e1e27891dba25ed715130fc6af1c71238
Robin <dev@pulsarr.fr>
2024-01-04 08:41:50 +0100

    Fix: give orginal name of column in affichage if columnLabel has been unchecked

--- apps/server/src/modules/demarches/utils/demarche.utils.ts
+++ apps/server/src/modules/demarches/utils/demarche.utils.ts
@@ -6,19 +6,29 @@ const fromMappingColumnArrayToHashFactory =
   <T>(transformFn: (column: MappingColumn) => T) =>
     (mappingColumns: MappingColumn[]): MappingColumnToHash<T> => {
       return Object.fromEntries(
-        mappingColumns.map(mc =>
-          mc.children?.length
-            ? mc.children.map(mc2 => [mc2.id, transformFn(mc2)])
-            : [[mc.id, transformFn(mc)]],
-        ).flat(1),
+        mappingColumns
+          .map((mc) =>
+            mc.children?.length
+              ? mc.children.map((mc2) => [mc2.id, transformFn(mc2)])
+              : [[mc.id, transformFn(mc)]],
+          )
+          .flat(1),
       )
     }
 
-const identity: <T>(x: T) => T = x => x
-const prop = <P extends keyof MappingColumn>(p: P) => (x: MappingColumn): MappingColumn[P] => x[p]
+const identity: <T>(x: T) => T = (x) => x
+const prop =
+  <P extends keyof MappingColumn>(p: P, p2?: P) =>
+    (x: MappingColumn): MappingColumn[P] =>
+      x[p] || x[p2]
 
-export const fromMappingColumnArrayToHash = fromMappingColumnArrayToHashFactory(identity)
+export const fromMappingColumnArrayToHash =
+  fromMappingColumnArrayToHashFactory(identity)
 
-export const fromMappingColumnArrayToTypeHash = fromMappingColumnArrayToHashFactory<FieldTypeKeys>(prop('type'))
+export const fromMappingColumnArrayToTypeHash =
+  fromMappingColumnArrayToHashFactory<FieldTypeKeys>(prop('type'))
 
-export const fromMappingColumnArrayToLabelHash = fromMappingColumnArrayToHashFactory<string>(prop('columnLabel'))
+export const fromMappingColumnArrayToLabelHash =
+  fromMappingColumnArrayToHashFactory<string>(
+    prop('columnLabel', 'originalLabel'),
+  )
