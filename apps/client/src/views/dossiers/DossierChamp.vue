b3753cda83bb521c94f6e89e58fbfb960338c793
Robin <dev@pulsarr.fr>
2024-01-08 15:37:42 +0100

    fix: oui ou non instead of false or true in fiche dossier

--- apps/client/src/views/dossiers/DossierChamp.vue
+++ apps/client/src/views/dossiers/DossierChamp.vue
@@ -6,7 +6,7 @@ import type { PieceJustificativeChamp, RepetitionChamp } from '@biblio-num/share
 import DossierChamps from './DossierChamps.vue'
 
 defineProps<{
-  champ: PieceJustificativeChamp & RepetitionChamp
+  champ: PieceJustificativeChamp & RepetitionChamp;
 }>()
 </script>
 
@@ -18,26 +18,41 @@ defineProps<{
   >
     <hr class="fr-mt-3w fr-pb-1w">
     <div class="fr-mx-7w">
+      <!-- LABEL -->
       <label
         :for="champ.id"
         class="fr-text--bold fr-text--lg bn-champ--text"
-      > {{ champ.label }}</label>
+      >
+        {{ champ.label }}
+      </label>
+      <!-- SI Piece jointe -->
       <div
-        v-if="champ.__typename =='PieceJustificativeChamp' "
+        v-if="champ.__typename == 'PieceJustificativeChamp'"
         :id="champ.id"
         class="fr-text fr-mt-1w bn-champ--text"
       >
         <a
           download
-          :href=" champ.file?.url"
+          :href="champ.file?.url"
           target="_blank"
           class="fr-link"
         >{{ champ.file?.filename }} <span class="fr-text--xs">({{ prettyByteSizeByString(String(champ.file?.byteSizeBigInt)) }})</span></a>
       </div>
+      <!-- SI Repetable -->
       <DossierChamps
-        v-else-if="champ.__typename =='RepetitionChamp' "
+        v-else-if="champ.__typename == 'RepetitionChamp'"
         :champs="champ.champs"
       />
+      <!-- Si boolean -->
+      <div
+        v-if="champ.__typename == 'CheckboxChamp'"
+        :id="champ.id"
+        class="fr-text fr-mt-1w bn-champ--text"
+      >
+        {{ champ.stringValue === "true" ? "Oui" : "Non" }}
+      </div>
+
+      <!-- Par défault -->
       <div
         v-else
         :id="champ.id"
