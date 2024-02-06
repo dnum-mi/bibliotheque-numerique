00a71646b0bc228251d9b9a06b3f43685ad81d48
Robin <dev@pulsarr.fr>
2024-02-06 16:23:12 +0100

    chore: cron jobs are now added from api and typings for jobs are created

--- apps/server/src/apps/worker-file/worker-file.module.ts
+++ apps/server/src/apps/worker-file/worker-file.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigModuleOptions } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
-import configuration from '../../config/worker.config'
+import configuration from '../../config/cron.config'
 import loggerConfig from '../../config/logger.config'
 
 import { LoggerModule } from '@/shared/modules/logger/logger.module'
 import { typeormFactoryLoader } from '@/shared/utils/typeorm-factory-loader'
 import redisConfig from '@/config/redis.config'
 import { CustomBullModule } from '@/shared/modules/custom-bull/custom-bull.module'
-import { QueueName } from '@/shared/modules/custom-bull/objects/const/queues-name.const'
+import { QueueName } from '@/shared/modules/custom-bull/objects/const/queues-name.enum'
 import { BullModule, BullModuleOptions } from '@nestjs/bull'
 
 @Module({
--- apps/server/src/modules/cron/mapper-jobs.type.ts
+++
@@ -1,8 +0,0 @@
-export type TMapperJob = {
-  name: string;
-  cronTime: string;
-  fct: () => Promise<void>;
-  description: string;
-};
-
-export type TMapperJobs = TMapperJob[];
