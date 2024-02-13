48909d3743e81875369516056c67e3c1fc26b60a
baudointran <baudointran@users.noreply.github.com>
2024-02-13 15:46:39 +0100

    chore: update client to move path of organisme-type.enum

--- apps/client/src/components/Badges/OrganismeBadge.vue
+++ apps/client/src/components/Badges/OrganismeBadge.vue
@@ -1,5 +1,5 @@
 <script lang='ts' setup>
-import type { OrganismeTypeKeys } from '@biblio-num/shared'
+import type { OrganismeTypeKeys } from '@biblio-num/shared-utils'
 
 defineProps<{
   type: OrganismeTypeKeys
