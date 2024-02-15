8cfaca20c5f94b65dff4cdbf1ecffd3847e3036f
baudointran <baudointran@users.noreply.github.com>
2024-02-15 14:49:08 +0100

    chore: fix config to build shared and shared-utils

--- packages/shared-utils/tsup.config.ts
+++ packages/shared-utils/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   splitting: false,
-  sourcemap: true,
   experimentalDts: true,
   clean: true,
   format: ['cjs', 'esm'],
