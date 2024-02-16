bb9163f75e50a7b2556808992f15fcfdb0c2a662
baudointran <baudointran@users.noreply.github.com>
2024-02-16 21:31:58 +0100

    chore: fix linting

--- apps/server/src/shared/pagination/column-contains-key.contraint.ts
+++ apps/server/src/shared/pagination/column-contains-key.contraint.ts
@@ -7,7 +7,7 @@ import { SortDto } from './sort.dto'
 import { CreateCustomFilterDto } from '@/modules/custom-filters/objects/dtos/create-custom-filter.dto'
 
 // reduceExistInColumn
-const reic = (array: string[] | undefined, o: CreateCustomFilterDto) => {
+const reic = (array: string[] | undefined, o: CreateCustomFilterDto):boolean => {
   return (array || []).reduce(
     (acc, curr) => acc && o.columns.includes(curr),
     true,
@@ -16,7 +16,7 @@ const reic = (array: string[] | undefined, o: CreateCustomFilterDto) => {
 
 @ValidatorConstraint({ async: false })
 export class ColumnContainsKeyContraint implements ValidatorConstraintInterface {
-  validate (useless: unknown, args: ValidationArguments) {
+  validate (useless: unknown, args: ValidationArguments):boolean {
     const o: CreateCustomFilterDto = args.object as CreateCustomFilterDto
     const propertyName = args.property as keyof CreateCustomFilterDto
     let array: string[] = []
@@ -31,7 +31,8 @@ export class ColumnContainsKeyContraint implements ValidatorConstraintInterface 
     return reic(array, o)
   }
 
-  defaultMessage (args: ValidationArguments) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  defaultMessage (args: ValidationArguments):string {
     return 'The selected columns does not contain the column you are trying to use.'
   }
 }
