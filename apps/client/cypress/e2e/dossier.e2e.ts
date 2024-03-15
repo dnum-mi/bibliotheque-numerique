be3458dd6b53ac88c8a50b7a0297e3cd0b2b64be
baudointran <baudointran@users.noreply.github.com>
2024-03-15 12:07:07 +0100

    fix: display correctly tab files in fiche dossier

---
+++ apps/client/cypress/e2e/dossier.e2e.ts
@@ -0,0 +1,24 @@
+import dosiser from '../fixtures/dossier.json'
+import files from '../fixtures/files.json'
+describe('Vue Dossier', () => {
+  beforeEach(() => {
+    cy.fixture('admin-profile').then((user) => {
+      cy.intercept('api/users/me', user).as('me')
+    })
+    cy.intercept('api//health', { info: { } })
+  })
+
+  it('PJ', () => {
+    cy.intercept({ method: 'GET', url: '/api/dossiers/1' }, dosiser).as('dossier')
+    cy.intercept({ method: 'POST', url: '/api/dossiers/1/files/list' }, files).as('files')
+
+    cy.visit('/dossiers/1')
+    cy.wait('@dossier')
+    cy.get('.fr-tabs')
+      .should('contain', 'Pièces jointes')
+      .contains('Pièces jointes')
+      .click()
+    cy.wait('@files')
+    cy.get('.ag-row').should('have.length', 6)
+  })
+})
