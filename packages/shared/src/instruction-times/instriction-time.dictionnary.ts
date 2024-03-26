10d90dad2d00f1b943685b2a6db49bcddadab2a7
baudointran <baudointran@users.noreply.github.com>
2024-03-26 18:17:18 +0100

    chore: update dictionnary instruction-time to set type in filter renderer

--- packages/shared/src/instruction-times/instriction-time.dictionnary.ts
+++ packages/shared/src/instruction-times/instriction-time.dictionnary.ts
@@ -1,13 +1,29 @@
+import { createEnum } from "../factories";
 import { EInstructionTimeState } from "./intruction-time.type";
 
+export const instructionTimeValues = [
+'1ère demande',
+'1ère demande',
+'Intention opposition',
+'Erreur',
+'Prorogation',
+'Instruction',
+'Délai expiré',
+'2ème demande',
+'2ème demande',
+] as const
+
+export type InstructionTimeValueKey = (typeof instructionTimeValues)[number]
+export const eInstructionTimeValue = createEnum(instructionTimeValues)
+
 export const instructionTimeValueDictionary = {
-  [EInstructionTimeState.FIRST_REQUEST]: '1ère demande',
-  [EInstructionTimeState.FIRST_RECEIPT]: '1ère demande',
-  [EInstructionTimeState.INTENT_OPPO]: 'Intention opposition',
-  [EInstructionTimeState.IN_ERROR]: 'Erreur',
-  [EInstructionTimeState.IN_EXTENSION]: 'Prorogation',
-  [EInstructionTimeState.IN_PROGRESS]: 'Instruction',
-  [EInstructionTimeState.OUT_OF_DATE]: 'Délai expiré',
-  [EInstructionTimeState.SECOND_RECEIPT]: '2ème demande',
-  [EInstructionTimeState.SECOND_REQUEST]: '2ème demande',
+  [EInstructionTimeState.FIRST_REQUEST]: eInstructionTimeValue['1ère demande'],
+  [EInstructionTimeState.FIRST_RECEIPT]: eInstructionTimeValue['1ère demande'],
+  [EInstructionTimeState.INTENT_OPPO]: eInstructionTimeValue['Intention opposition'],
+  [EInstructionTimeState.IN_ERROR]: eInstructionTimeValue['Erreur'],
+  [EInstructionTimeState.IN_EXTENSION]: eInstructionTimeValue['Prorogation'],
+  [EInstructionTimeState.IN_PROGRESS]: eInstructionTimeValue['Instruction'],
+  [EInstructionTimeState.OUT_OF_DATE]: eInstructionTimeValue['Délai expiré'],
+  [EInstructionTimeState.SECOND_RECEIPT]: eInstructionTimeValue['2ème demande'],
+  [EInstructionTimeState.SECOND_REQUEST]: eInstructionTimeValue['2ème demande'],
 } as const
