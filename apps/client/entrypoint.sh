6935a08a40fe3749ee537e86aaed12ebef47a9ce
pengfei.dong <mail@peng-fei.name>
2024-03-26 14:21:00 +0100

    fix: debug sed cmd in entrypoint.sh

--- apps/client/entrypoint.sh
+++ apps/client/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i 's#__AG_GRID_LICENSE_KEY__#'$AG_GRID_LICENSE_KEY'#g' /app/assets/index*.js
+sed -i "s#__AG_GRID_LICENSE_KEY__#$AG_GRID_LICENSE_KEY#g" /app/assets/index*.js
 
 /opt/bitnami/scripts/nginx/run.sh
