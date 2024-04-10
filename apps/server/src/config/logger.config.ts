72852a218643444b326405c3acc5159b8eb8697f
Robin <dev@pulsarr.fr>
2024-04-10 10:13:46 +0200

    fix: all log true by default

--- apps/server/src/config/logger.config.ts
+++ apps/server/src/config/logger.config.ts
@@ -4,6 +4,6 @@ export default registerAs('log', () => ({
   log: process.env.LOG_LEVEL_LOG === 'true' || true,
   warn: process.env.LOG_LEVEL_WARN === 'true' || true,
   error: process.env.LOG_LEVEL_ERROR === 'true' || true,
-  verbose: process.env.LOG_LEVEL_VERBOSE === 'true' || false,
-  debug: process.env.LOG_LEVEL_DEBUG === 'true' || false,
+  verbose: process.env.LOG_LEVEL_VERBOSE === 'true' || true,
+  debug: process.env.LOG_LEVEL_DEBUG === 'true' || true,
 }))
