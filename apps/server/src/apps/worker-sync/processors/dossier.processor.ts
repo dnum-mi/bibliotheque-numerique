d4fc4e767e599e73ed9d762b48725b2550069ded
baudointran <baudointran@users.noreply.github.com>
2024-06-19 16:23:05 +0200

    fix: cron job run by event

--- apps/server/src/apps/worker-sync/processors/dossier.processor.ts
+++ apps/server/src/apps/worker-sync/processors/dossier.processor.ts
@@ -6,14 +6,16 @@ import { Job } from 'bull'
 import { LoggerService } from '@/shared/modules/logger/logger.service'
 import { DossierSynchroniseService } from '@/modules/dossiers/providers/synchronization/dossier-synchronise.service'
 import { DemarcheService } from '@/modules/demarches/providers/services/demarche.service'
-import { DsApiClient } from '@dnum-mi/ds-api-client'
+import { DossierState, DsApiClient } from '@dnum-mi/ds-api-client'
 import { AsyncLocalStorage } from 'async_hooks'
 import { AsyncLocalStore } from '@/shared/modules/als/async-local-store.type'
 import { ALS_INSTANCE } from '@/shared/modules/als/als.module'
 import { Inject } from '@nestjs/common'
 import { DossierService } from '@/modules/dossiers/providers/dossier.service'
-import { IsNull, LessThan, Not } from 'typeorm'
+import { FindManyOptions, In, IsNull, LessThan, Not } from 'typeorm'
 import { addMonths } from 'date-fns'
+import { eAnonymisationEvent } from '@biblio-num/shared'
+import { Dossier } from '../../../modules/dossiers/objects/entities/dossier.entity'
 
 @Processor(QueueName.sync)
 export class DossierProcessor {
@@ -61,17 +63,51 @@ export class DossierProcessor {
       const now = new Date()
       for (const demarche of demarches) {
         const limitDate = addMonths(now, -demarche.nbrMonthAnonymisation)
-        const dossiers = await this.dossierService.repository.find({
+        const whereQuery: FindManyOptions<Dossier>['where'] = {}
+        switch (demarche.anonymizationEvent) {
+        case eAnonymisationEvent.DepotDate:
+          whereQuery.dateDepot = LessThan(limitDate)
+          break
+        case eAnonymisationEvent.AcceptedDate:
+          whereQuery.dateTraitement = LessThan(limitDate)
+          whereQuery.state = DossierState.Accepte
+          break
+        case eAnonymisationEvent.DecisionDate:
+          whereQuery.dateTraitement = LessThan(limitDate)
+          whereQuery.state = In([DossierState.Accepte, DossierState.Refuse, DossierState.SansSuite])
+          break
+        default:
+          this.logger.warn(`unknown event (${
+            demarche.anonymizationEvent}) to anonymise demarche ${demarche.dsDataJson.number}`)
+          continue
+        }
+
+        let dossiers = await this.dossierService.repository.find({
           where: {
             anonymisedAt: null,
             demarcheId: demarche.id,
-            dateDepot: LessThan(limitDate),
+            ...whereQuery,
           },
           select: ['id', 'dsDataJson'],
         })
+
+        if (demarche.isOnAllDossiersOfOrganisme) {
+          const dossiersOfOrg = await this.dossierService.repository.find({
+            where: {
+              anonymisedAt: null,
+              demarcheId: In(demarches.map(d => d.id)),
+              organisme: { id: In([...new Set(dossiers.map(d => d.organisme.id))]) },
+            },
+
+            select: ['id', 'dsDataJson'],
+          })
+          dossiers = dossiers.concat(dossiersOfOrg)
+        }
+
         const dossierStep = oneDemarcheStep / dossiers.length
         for (const dossier of dossiers) {
           await this.dossierService.anonymiseDossierDemandeur(dossier)
+
           job.progress(job.progress() + dossierStep)
         }
       }
