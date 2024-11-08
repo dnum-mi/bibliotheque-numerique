196a5910cb09fb563e604bb075ad50b2b743537f
baudointran <baudointran@users.noreply.github.com>
2024-11-08 17:56:01 +0100

    fix: rename variable env of siaf

--- apps/server/src/config/siaf.config.ts
+++ apps/server/src/config/siaf.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config'
 
-export default registerAs('siaf', () => ({
-  url: process.env.SIAF_API_URL,
-  key: process.env.SIAF_API_KEY,
+export default registerAs('siafHub', () => ({
+  url: process.env.SIAF_HUB_API_URL,
+  key: process.env.SIAF_HUB_API_KEY,
 }))
