f62b7431a40b24a0b3515a1a9a1c9c4bd3fe53f8
Antoine Grandiere <antoinegrandiere@Antoines-MBP.lan>
2025-02-04 11:48:58 +0100

    feat: improve wording

--- apps/server/src/config/auth.config.ts
+++ apps/server/src/config/auth.config.ts
@@ -4,5 +4,5 @@ export default registerAs('auth', () => ({
   client_id: process.env.AUTH_CLIENT_ID,
   client_secret: process.env.AUTH_CLIENT_SECRET,
   redirect_uri: process.env.AUTH_REDIRECT_URI,
-  discoveryUrl: process.env.AUTH_DISCOVERY_URL,
+  discovery_url: process.env.AUTH_DISCOVERY_URL,
 }))
