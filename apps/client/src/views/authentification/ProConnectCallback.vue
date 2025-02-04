ff32036ef0c5cdc9990bfa1a3358ac1687979c93
pengfei.dong <mail@peng-fei.name>
2025-02-04 19:18:01 +0100

    fix: update openid-client version for proconnect, and gestion error for client

--- apps/client/src/views/authentification/ProConnectCallback.vue
+++ apps/client/src/views/authentification/ProConnectCallback.vue
@@ -9,12 +9,19 @@ const route = useRoute()
 const userStore = useUserStore()
 
 const proConnectCallback = async () => {
-  const code = route.query.code as string | undefined
-  const state = route.query.state as string | undefined
-  const iss = route.query.iss as string | undefined
-  if (code && state && iss) {
-    await userStore.loginWithProconnect(code, state, iss)
-    router.push({ name: routeNames.DEMARCHES })
+  try {
+    const code = route.query.code as string | undefined
+    const state = route.query.state as string | undefined
+    const iss = route.query.iss as string | undefined
+    if (code && state && iss) {
+      await userStore.loginWithProconnect(code, state, iss)
+      router.push({ name: routeNames.DEMARCHES })
+    } else {
+      throw new Error('Invalid proconnect code, state or iss')
+    }
+  } catch (e) {
+    console.error(e)
+    router.push({ name: routeNames.SIGNIN })
   }
 }
 
