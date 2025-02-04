25a1b1275b1588874f9311b0f7a2d58f58a08d5b
pengfei.dong <mail@peng-fei.name>
2025-02-04 19:16:04 +0100

    fix: debug ci, add default conf for proconnect

--- apps/server/src/config/auth.config.ts
+++ apps/server/src/config/auth.config.ts
@@ -1,8 +1,8 @@
 import { registerAs } from '@nestjs/config'
 
 export default registerAs('auth', () => ({
-  client_id: process.env.AUTH_CLIENT_ID,
-  client_secret: process.env.AUTH_CLIENT_SECRET,
-  redirect_uri: process.env.AUTH_REDIRECT_URI,
-  discovery_url: process.env.AUTH_DISCOVERY_URL,
+  client_id: process.env.AUTH_CLIENT_ID || 'client_id',
+  client_secret: process.env.AUTH_CLIENT_SECRET || 'client_secret',
+  redirect_uri: process.env.AUTH_REDIRECT_URI || 'http://localhost:3000/login-callback',
+  discovery_url: process.env.AUTH_DISCOVERY_URL || 'https://app-sandbox.moncomptepro.beta.gouv.fr',
 }))
