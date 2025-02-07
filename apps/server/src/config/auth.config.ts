cc76173ef55976d7e02e8bb9bc18ad8e38114052
baudointran <baudointran@users.noreply.github.com>
2025-02-07 16:32:03 +0100

    fix: use algoritm json for open-id connect

--- apps/server/src/config/auth.config.ts
+++ apps/server/src/config/auth.config.ts
@@ -5,4 +5,5 @@ export default registerAs('auth', () => ({
   client_secret: process.env.AUTH_CLIENT_SECRET || 'client_secret',
   redirect_uri: process.env.AUTH_REDIRECT_URI || 'http://localhost:3000/login-callback',
   discovery_url: process.env.AUTH_DISCOVERY_URL || 'https://app-sandbox.moncomptepro.beta.gouv.fr',
+  userinfo_signed_response_alg: process.env.AUTH_USERINFO_SIGNED_RESPONSE_ALG || 'RS256',
 }))
