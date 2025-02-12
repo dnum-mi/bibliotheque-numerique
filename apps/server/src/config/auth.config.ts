3c5d068105e05f9a3fa2adf167b1b6d254d43926
Antoine Grandiere <antoinegrandiere@Antoines-MBP.lan>
2025-02-12 11:54:50 +0100

    fix: update sso discovery url

--- apps/server/src/config/auth.config.ts
+++ apps/server/src/config/auth.config.ts
@@ -4,7 +4,7 @@ export default registerAs('auth', () => ({
   client_id: process.env.AUTH_CLIENT_ID || 'client_id',
   client_secret: process.env.AUTH_CLIENT_SECRET || 'client_secret',
   redirect_uri: process.env.AUTH_REDIRECT_URI || 'http://localhost:3000/login-callback',
-  discovery_url: process.env.AUTH_DISCOVERY_URL || 'https://app-sandbox.moncomptepro.beta.gouv.fr',
+  discovery_url: process.env.AUTH_DISCOVERY_URL || 'https://identite-sandbox.proconnect.gouv.fr',
   userinfo_signed_response_alg: process.env.AUTH_USERINFO_SIGNED_RESPONSE_ALG || 'RS256',
   enable_sso: process.env.AUTH_ENABLE_SSO || true,
 }))
