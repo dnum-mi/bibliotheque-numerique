55581c313da2c0907ebac12c55c3573b33676afe
Antoine Grandiere <antoinegrandiere@Antoines-MacBook-Pro.local>
2025-03-04 19:48:27 +0100

    fix: adress for associations

--- apps/client/src/views/organismes/search/searchOrganismes.vue
+++ apps/client/src/views/organismes/search/searchOrganismes.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { organismeTypes, EEntityTypeSearchOrganisme } from '@biblio-num/shared'
-import type { ISiafSearchOrganismeResponseOutput } from '@biblio-num/shared'
+import type { ISiafAssociationEntity, ISiafFondationEntity, ISiafSearchOrganismeResponseOutput } from '@biblio-num/shared'
 import apiClient from '../../../api/api-client'
 import { routeNames } from '../../../router/route-names'
 import { EOrganismeIdType } from '../../../stores'
@@ -26,14 +26,34 @@ const rows = computed(() => {
   return results?.value?.map((result) => {
     const id = result.entity.id
     const idType: OrganismeIdType
-  = (result.entity_type as EEntityTypeSearchOrganisme) === EEntityTypeSearchOrganisme.fondation
-    ? EOrganismeIdType.Rnf
-    : EOrganismeIdType.Rna
-    const subType = result.entity.foundationType || organismeTypes[5]
-    const address = result.entity.address?.dsStringValue || ''
-    const codePostalMatch = address.match(/\b\d{5}\b/)
-    const codePostal = codePostalMatch ? codePostalMatch[0] : 'N/A'
-    const ville = codePostalMatch ? address.split(codePostalMatch[0])[1].trim() : 'N/A'
+      = (result.entity_type as EEntityTypeSearchOrganisme) === EEntityTypeSearchOrganisme.Fondation
+        ? EOrganismeIdType.Rnf
+        : EOrganismeIdType.Rna
+
+    const subType
+      = result.entity_type === EEntityTypeSearchOrganisme.Fondation
+        ? (result.entity as ISiafFondationEntity).foundationType || organismeTypes[5]
+        : organismeTypes[5]
+
+    let codePostal = 'N/A'
+    let ville = 'N/A'
+
+    if (result.entity_type === 'fondation') {
+      const entity = result.entity as ISiafFondationEntity
+      const address = entity.address?.dsStringValue || ''
+      const codePostalMatch = address.match(/\b\d{5}\b/)
+      codePostal = codePostalMatch ? codePostalMatch[0] : 'N/A'
+      ville = codePostalMatch ? address.split(codePostalMatch[0])[1].trim() : 'N/A'
+    } else if (result.entity_type === 'association') {
+      const entity = result.entity as ISiafAssociationEntity
+      const addresses = entity.addresses
+
+      if (addresses.length > 0 && addresses[0].rnaAddress?.address) {
+        const rnaAddress = addresses[0].rnaAddress.address
+        codePostal = rnaAddress.codepostal || 'N/A'
+        ville = rnaAddress.achemine || 'N/A'
+      }
+    }
 
     return {
       rowData: [
@@ -45,7 +65,6 @@ const rows = computed(() => {
         },
         ville,
         codePostal,
-
         {
           component: 'DsfrButton',
           label: 'Voir',
--- packages/shared/src/organismes/hub-search-organisme-output.interface.ts
+++ packages/shared/src/organismes/hub-search-organisme-output.interface.ts
@@ -1,92 +1,145 @@
-import { ISiafAssociationOutput, ISiafFondationOutput } from "./hub-output.interface"
-
-
 export enum EEntityTypeSearchOrganisme {
-  association,
-  fondation
+  Association = "association",
+  Fondation = "fondation"
 }
+
 export interface ISiafSearchOrganismeResponseOutput {
   score: number;
   entity_type: EEntityTypeSearchOrganisme;
-  entity: {
-    id: string;
-    title: string;
-    email: string;
-    phone: string;
-    foundationType: string;
-    department: string;
-    originalDepartment: string;
-    createdAt: string;
-    updatedAt: string;
-    originalCreatedAt: string;
-    fiscalEndDateAt: string;
-    declarationYears: string[];
-    dissolved: {
-      otherFiles: string[];
-    };
-    status: {
-      file: {
-        id: string;
-        name: string;
-        checksum: string;
-        byteSize: number;
-        mimeType: string;
-      };
-    };
+  entity: ISiafFondationEntity | ISiafAssociationEntity;
+}
+
+export interface ISiafAssociationEntity {
+  id: string;
+  title: string;
+  objectDescription: string;
+  nature: string;
+  emails: string[];
+  phones: string[];
+  websites: string[];
+  siret: string | null;
+  createdAt: string;
+  updatedAt: string;
+  dissolved: DissolvedAssociation;
+  addresses: AssociationAddress[];
+}
+
+export interface ISiafFondationEntity {
+  id: string;
+  title: string;
+  email: string;
+  phone: string;
+  foundationType: string;
+  department: string;
+  originalDepartment: string;
+  createdAt: string;
+  updatedAt: string;
+  originalCreatedAt: string;
+  fiscalEndDateAt: string;
+  declarationYears: string[];
+  dissolved: Dissolved;
+  status: Status;
+  address: FondationAddress;
+  persons: Person[];
+  objectDescription: string;
+  generalInterest: string;
+  internationalAction: boolean;
+  version: number;
+  previous_versions: [];
+}
+
+export interface FondationAddress {
+  dsStringValue: string;
+  coordinates: [number | null, number | null];
+}
+
+export interface AssociationAddress {
+  coordinates: [number | null, number | null];
+  rnaAddress: {
+    kind: string;
     address: {
-      dsStringValue: string;
-      coordinates: [number | null, number | null];
+      declarant: string;
+      complemid: string;
+      complemgeo: string;
+      libvoie: string;
+      distrib: string;
+      codepostal: string;
+      achemine: string;
+      pays: string;
     };
-    persons: {
-      lastName: string;
-      firstName: string;
-      profession: string;
-      nationality: string;
-      bornAt: string;
-      isFounder: boolean;
-      role: string;
-      residenceCountry: string;
-      entryDate: string;
-      jobPosition: string;
-      address: {
-        dsStringValue: string;
-        coordinates: [number | null, number | null];
-        dsAddress: {
-          label: string;
-          type: string;
-          streetAddress: string;
-          streetNumber: string;
-          streetName: string;
-          postalCode: string;
-          cityName: string;
-          cityCode: string;
-          departmentName: string;
-          departmentCode: string;
-          regionName: string;
-          regionCode: string;
-        };
-        gouvAddress: {
-          label: string;
-          housenumber: string;
-          name: string;
-          postcode: string;
-          citycode: string;
-          city: string;
-          context: string;
-          type: string;
-          street: string;
-        };
-      };
-    }[];
-    objectDescription: string;
-    generalInterest: string;
-    internationalAction: boolean;
-    version: number;
-    previous_versions: string[];
   };
 }
 
+export interface Dissolved {
+  otherFiles: string[];
+}
+
+export interface DissolvedAssociation {
+  dissolvedAt: string | null;
+  file: string | null;
+}
+
+export interface Status {
+  file: FileData;
+}
+
+export interface FileData {
+  id: string;
+  name: string;
+  checksum: string;
+  byteSize: number;
+  mimeType: string;
+}
+
+export interface Person {
+  lastName: string;
+  firstName: string;
+  profession: string;
+  nationality: string;
+  bornAt: string;
+  isFounder: boolean;
+  role: string;
+  residenceCountry: string;
+  entryDate: string;
+  jobPosition: string;
+  address: PersonAddress;
+}
+
+export interface PersonAddress {
+  dsStringValue: string;
+  coordinates: [number | null, number | null];
+  dsAddress: DsAddress;
+  gouvAddress: GouvAddress;
+}
+
+export interface DsAddress {
+  label: string;
+  type: string;
+  streetAddress: string;
+  streetNumber: string;
+  streetName: string;
+  postalCode: string;
+  cityName: string;
+  cityCode: string;
+  departmentName: string;
+  departmentCode: string;
+  regionName: string;
+  regionCode: string;
+}
+
+export interface GouvAddress {
+  label: string;
+  housenumber: string;
+  name: string;
+  postcode: string;
+  citycode: string;
+  city: string;
+  context: string;
+  type: string;
+  street: string;
+}
+
 export interface ISiafSearchOrganismeOutput {
-  sentence: string,
-  search_response: ISiafSearchOrganismeResponseOutput[]
+  sentence: string;
+  search_response: ISiafSearchOrganismeResponseOutput[];
 }
