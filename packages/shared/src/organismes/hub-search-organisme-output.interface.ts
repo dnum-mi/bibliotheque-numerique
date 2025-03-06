d34c2b9c0ceabeac7e0f4152ce9a98b6bfc33369
Antoine Grandiere <antoinegrandiere@Antoines-MacBook-Pro.local>
2025-03-06 11:11:49 +0100

    fix: typescript interface

--- packages/shared/src/organismes/hub-search-organisme-output.interface.ts
+++ packages/shared/src/organismes/hub-search-organisme-output.interface.ts
@@ -1,3 +1,6 @@
+import { ISiafRnaOutput } from "./siaf/siaf-rna-output.interface";
+import { ISiafRnfOutput } from "./siaf/siaf-rnf-output.interface";
+
 export enum EEntityTypeSearchOrganisme {
   Association = "association",
   Fondation = "fondation"
@@ -6,137 +9,7 @@ export enum EEntityTypeSearchOrganisme {
 export interface ISiafSearchOrganismeResponseOutput {
   score: number;
   entity_type: EEntityTypeSearchOrganisme;
-  entity: ISiafFondationEntity | ISiafAssociationEntity;
-}
-
-export interface ISiafAssociationEntity {
-  id: string;
-  title: string;
-  objectDescription: string;
-  nature: string;
-  emails: string[];
-  phones: string[];
-  websites: string[];
-  siret: string | null;
-  createdAt: string;
-  updatedAt: string;
-  dissolved: DissolvedAssociation;
-  addresses: AssociationAddress[];
-}
-
-export interface ISiafFondationEntity {
-  id: string;
-  title: string;
-  email: string;
-  phone: string;
-  foundationType: string;
-  department: string;
-  originalDepartment: string;
-  createdAt: string;
-  updatedAt: string;
-  originalCreatedAt: string;
-  fiscalEndDateAt: string;
-  declarationYears: string[];
-  dissolved: Dissolved;
-  status: Status;
-  address: FondationAddress;
-  persons: Person[];
-  objectDescription: string;
-  generalInterest: string;
-  internationalAction: boolean;
-  version: number;
-  previous_versions: [];
-}
-
-export interface FondationAddress {
-  dsStringValue: string;
-  coordinates: [number | null, number | null];
-}
-
-export interface AssociationAddress {
-  coordinates: [number | null, number | null];
-  rnaAddress: {
-    kind: string;
-    address: {
-      declarant: string;
-      complemid: string;
-      complemgeo: string;
-      libvoie: string;
-      distrib: string;
-      codepostal: string;
-      achemine: string;
-      pays: string;
-    };
-  };
-}
-
-export interface Dissolved {
-  otherFiles: string[];
-}
-
-export interface DissolvedAssociation {
-  dissolvedAt: string | null;
-  file: string | null;
-}
-
-export interface Status {
-  file: FileData;
-}
-
-export interface FileData {
-  id: string;
-  name: string;
-  checksum: string;
-  byteSize: number;
-  mimeType: string;
-}
-
-export interface Person {
-  lastName: string;
-  firstName: string;
-  profession: string;
-  nationality: string;
-  bornAt: string;
-  isFounder: boolean;
-  role: string;
-  residenceCountry: string;
-  entryDate: string;
-  jobPosition: string;
-  address: PersonAddress;
-}
-
-export interface PersonAddress {
-  dsStringValue: string;
-  coordinates: [number | null, number | null];
-  dsAddress: DsAddress;
-  gouvAddress: GouvAddress;
-}
-
-export interface DsAddress {
-  label: string;
-  type: string;
-  streetAddress: string;
-  streetNumber: string;
-  streetName: string;
-  postalCode: string;
-  cityName: string;
-  cityCode: string;
-  departmentName: string;
-  departmentCode: string;
-  regionName: string;
-  regionCode: string;
-}
-
-export interface GouvAddress {
-  label: string;
-  housenumber: string;
-  name: string;
-  postcode: string;
-  citycode: string;
-  city: string;
-  context: string;
-  type: string;
-  street: string;
+  entity: ISiafRnaOutput | ISiafRnfOutput
 }
 
 export interface ISiafSearchOrganismeOutput {
