5082da333acd3107acac195ecee6b399f4d5e1c6
Antoine Grandiere <antoinegrandiere@Antoines-MBP.lan>
2025-03-10 23:39:36 +0100

    fix: remove typescript exceptions

--- apps/server/src/modules/organismes/providers/validators/rnf-id/rnf-id.constraint.ts
+++ apps/server/src/modules/organismes/providers/validators/rnf-id/rnf-id.constraint.ts
@@ -1,17 +1,15 @@
 // TODO: duplicate from rnf-server
-import { ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator'
+import { ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator'
 import { isRnfLuhnValid } from '@/shared/utils/rnf.utils'
 
 @ValidatorConstraint({ async: false })
 export class RnfIdConstraint implements ValidatorConstraintInterface {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  validate (text: string, args: ValidationArguments):boolean {
+  validate (text: string):boolean {
     if (!text) return false
     return text.length >= 15 && text.length <= 18 && isRnfLuhnValid(text)
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  defaultMessage (args: ValidationArguments):string {
+  defaultMessage ():string {
     return "Le numéro RNF n'est pas valide"
   }
 }
