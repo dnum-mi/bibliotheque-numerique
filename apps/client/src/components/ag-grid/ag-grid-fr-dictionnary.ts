70d33ccda3c447a10f72a9c1b33c454089a96728
pengfei.dong <mail@peng-fei.name>
2025-05-06 16:12:40 +0200

    fix: set text for blanks of ag grid filtre

--- apps/client/src/components/ag-grid/ag-grid-fr-dictionnary.ts
+++ apps/client/src/components/ag-grid/ag-grid-fr-dictionnary.ts
@@ -1,3 +1,5 @@
 export const localeTextAgGrid = {
+  blanks: 'Vide',
+  selectAll: 'Tout sélectionner',
   loadingError: 'Une erreur inconnue est survenue durant le chargement. Veuillez contacter votre administrateur',
 }
