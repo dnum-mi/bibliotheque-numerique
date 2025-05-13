c251b179345b407a0d6273305d145f3b66dbab11
Papa Ousmane NDIAYE <papaousmvne.ndiaye@gmail.com>
2025-05-13 10:02:04 +0200

    feat: add DossierSection and DossierSidemenu components with grouped champs functionality

---
+++ apps/client/src/views/dossiers/DossierSidemenu.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+defineProps<{
+  menuItems: { id: string; text: string; to: string }[]
+  smoothScroll: (id: string) => void
+}>()
+</script>
+
+<template>
+  <div
+    id="summary"
+    class="fr-col-12 fr-col-xl-3 sticky-sidemenu"
+  >
+    <ul class="fr-sidemenu__list fr-hidden fr-unhidden-xl">
+      <li
+        v-for="(item, index) in menuItems"
+        :key="index"
+        class="fr-sidemenu__item"
+      >
+        <a
+          :href="item.to"
+          class="fr-sidemenu__link"
+          :aria-controls="`section-content-${item.id}`"
+          @click.prevent="smoothScroll(item.id)"
+        >
+          {{ item.text }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.sticky-sidemenu {
+  padding-right: 2rem;
+  position: sticky;
+  top: 2em;
+  max-height: 100vh;
+}
+
+.fr-sidemenu__list {
+  list-style: none;
+}
+
+[href] {
+  background-image: none !important;
+  background-position: initial !important;
+  background-repeat: initial !important;
+  background-size: initial !important;
+}
+</style>
