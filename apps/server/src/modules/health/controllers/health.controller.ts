2d392f7339c85e6f95be2597df49fa39bd0c5da7
pengfei.dong <mail@peng-fei.name>
2025-07-02 22:55:01 +0200

    fix: add ds and hub check in health

--- apps/server/src/modules/health/controllers/health.controller.ts
+++ apps/server/src/modules/health/controllers/health.controller.ts
@@ -12,6 +12,8 @@ import { LoggerService } from '@/shared/modules/logger/logger.service'
 import { VersionHealthIndicator } from '@/modules/health/providers/version-health.indicator'
 import { EnvironmentHealthIndicator } from '@/modules/health/providers/environment-health.indicator'
 import { UsualApiOperation } from '@/shared/documentation/usual-api-operation.decorator'
+import { DsHealthIndicator } from '@/modules/health/providers/ds-health.indicator'
+import { HubHealthIndicator } from '@/modules/health/providers/hub-health.indicator'
 
 @ApiTags('Health')
 @Controller('health')
@@ -22,6 +24,8 @@ export class HealthController {
     private readonly logger: LoggerService,
     private versionIndicator: VersionHealthIndicator,
     private environmentIndicator: EnvironmentHealthIndicator,
+    private dsHealth: DsHealthIndicator,
+    private hubHealth: HubHealthIndicator,
   ) {
     this.logger.setContext(this.constructor.name)
   }
@@ -39,11 +43,29 @@ export class HealthController {
     this.logger.verbose('Health check')
 
     return this.health.check([
-      (): Promise<HealthIndicatorResult> => this.db.pingCheck('database'),
+      (): Promise<HealthIndicatorResult> =>
+        this.db.pingCheck('database'),
       (): Promise<HealthIndicatorResult> =>
         this.versionIndicator.isHealthy('version'),
       (): Promise<HealthIndicatorResult> =>
         this.environmentIndicator.isHealthy('environment'),
+      (): Promise<HealthIndicatorResult> =>
+        this.dsHealth.isHealthy('ds'),
+      (): Promise<HealthIndicatorResult> =>
+        this.hubHealth.isHealthy('hub'),
     ])
+  }
+
+  @UsualApiOperation({
+    summary: 'Vérifie la vivacité du serveur.',
+    method: 'GET',
+    minimumRole: 'aucun',
+    responseType: null,
+  })
+  @Get('liveliness')
+  @PublicRoute()
+  liveliness(): string {
+    this.logger.verbose('Liveliness check')
+    return 'OK'
   }
 }
