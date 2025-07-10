e80ff2474d2dee78800a812c472612989ca16f5c
baudointran <baudointran@users.noreply.github.com>
2025-07-10 10:22:21 +0200

    fix: add interface for entity SyncState

--- apps/server/src/shared/sync-state/objects/entities/base-entity-sync-state.entity.ts
+++ apps/server/src/shared/sync-state/objects/entities/base-entity-sync-state.entity.ts
@@ -1,8 +1,9 @@
 import { JoinColumn, OneToOne } from 'typeorm'
 import { BaseEntity } from '@/shared/base-entity/base.entity'
 import { SyncState } from './sync-state.entity'
+import { IEntityWithSyncState } from '@biblio-num/shared'
 
-export abstract class BaseEntitySyncState extends BaseEntity {
+export abstract class BaseEntitySyncState extends BaseEntity implements IEntityWithSyncState {
   @OneToOne(() => SyncState, { eager: true, onDelete: 'CASCADE' })
   @JoinColumn({ foreignKeyConstraintName: 'FK_JOIN_SYNC_STAT' })
   syncState: SyncState
