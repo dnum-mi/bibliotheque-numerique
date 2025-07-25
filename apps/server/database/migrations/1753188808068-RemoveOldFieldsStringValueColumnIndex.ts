2e5f28c62f32e0f993521439d213080b1babe076
Papa Ousmane NDIAYE <papaousmvne.ndiaye@gmail.com>
2025-07-25 14:09:48 +0200

    fix: update max characters on index fields.stringValue

--- apps/server/database/migrations/1753188808068-RemoveOldFieldsStringValueColumnIndex.ts
+++ apps/server/database/migrations/1753188808068-RemoveOldFieldsStringValueColumnIndex.ts
@@ -5,7 +5,7 @@ export class RemoveOldFieldsStringValueColumnIndex1753188808068 implements Migra
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP INDEX "public"."IDX_3dc571fcc8aad37ba2f8d7cfd1"`);
-        await queryRunner.query(`CREATE INDEX "IDX_fields_stringValue_prefix" ON "fields" (left("stringValue", 400))`)
+        await queryRunner.query(`CREATE INDEX "IDX_fields_stringValue_prefix" ON "fields" (left("stringValue", 2000))`)
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
